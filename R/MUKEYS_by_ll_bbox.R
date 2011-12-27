00e70cf12e8f610d4baa2239569e0c6cd47b371c
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2011-12-27 22:31:59 +0000

    new NASIS query functions, removed corresponding queries from aqp

---
+++ R/MUKEYS_by_ll_bbox.R
@@ -0,0 +1,23 @@
+
+# http://sdmdataaccess.nrcs.usda.gov/Spatial/SDMNAD83Geographic.wfs?Service=WFS&Version=1.0.0&Request=GetFeature&OutputFormat=XmlMukeyList&Typename=MapunitPolyNoGeometry&BBOX=-120.950129388,37.7972571005,-120.677685495,37.9766971606
+
+MUKEYS_by_ll_bbox <- function(bbox)
+  {
+  # check for required packages
+  if(!require(RCurl) | !require(XML))
+    stop('please install the `RCurl` and `XML` packages')
+  
+  bbox.text <- paste(bbox, collapse=',')
+  
+  u <- paste( 'http://sdmdataaccess.nrcs.usda.gov/Spatial/SDMNAD83Geographic.wfs?Service=WFS&Version=1.0.0&Request=GetFeature&OutputFormat=XmlMukeyList&Typename=MapunitPolyNoGeometry&BBOX=', bbox.text, sep='')
+  
+  html <- getURL(u) 
+  html.tree <- htmlTreeParse(html, useInternalNodes=TRUE, error=function(...){}) 
+
+  m <- getNodeSet(html.tree, '//mapunitkeylist')
+  m.val <- gsub("'", '', xmlValue(m[[1]]))
+  m.keys <- strsplit(m.val, ',')[[1]]
+  
+  return(m.keys)
+  }
+
