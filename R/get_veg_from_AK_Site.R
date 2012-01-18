345e4a0147ad233cc5d0863a31395735ff76a39e
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2012-01-18 17:25:24 +0000

    minor changes to accomodate R 2.14 build system. there is still a problem with vignette indexing, but that is fine as the vignette isnt done yet

--- R/get_veg_from_AK_Site.R
+++ R/get_veg_from_AK_Site.R
@@ -1,3 +1,5 @@
+
+# gets all veg records per site ID
 get_veg_from_AK_Site <- function(dsn) {
 	# basic query
 	q <- "SELECT VegStop.usiteid as site_id, localplant.lplantsym as plantsym, localplant.lplantname as plantname, vegetation.COVER as pct_cover
@@ -6,7 +8,7 @@ get_veg_from_AK_Site <- function(dsn) {
 	INNER JOIN localplant ON vegetation.lplantsym = localplant.lplantuid
 	ORDER BY VegStop.usiteid ASC;"
   
-	# setup connection to our pedon database
+	# setup connection to the AK Site database
 	channel <- odbcConnectAccess(dsn, readOnlyOptimize=TRUE)
 	
 	# exec query
