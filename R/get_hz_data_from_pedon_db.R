99b5ec27064c4e8976175bf4fd31a8d3800fd84b
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2012-01-18 23:47:38 +0000

    added proper handling of multiple textures / horizon for pedonPC. an SQL solution for NASIS would be ideal, but needs research

--- R/get_hz_data_from_pedon_db.R
+++ R/get_hz_data_from_pedon_db.R
@@ -1,20 +1,24 @@
+# ISSUES:
+# 1. working with MS Access-SQL is a serious limitation
+# 2. joining to tables with 1:many cardinality will result in multiple rows/horizon
+# 3. this can be overcome using CTE queries to local NASIS, but not Access
+# 4. cases where this happens:
+#    - multiple textures defined for a single horizon
+
 get_hz_data_from_pedon_db <- function(dsn) {
-	# this can be optimized
-	# RF calculation should be done in  a sub-query
 	q <- "SELECT pedon.peiid, phorizon.phiid, pedon.upedonid as pedon_id, phorizon.hzname, phorizon.hzdept, phorizon.hzdepb,
-  phorizon.claytotest as clay, phorizon.silttotest as silt, phorizon.sandtotest as sand, IIF(IsNULL(tx.choice), til.choice, tx.choice) as texture_class, phfield, eff.choice AS effervescence, l.labsampnum, IIF(IsNULL(f.total_frags_pct), 0, f.total_frags_pct) AS total_frags_pct
+  phorizon.claytotest as clay, phorizon.silttotest as silt, phorizon.sandtotest as sand, phfield, eff.choice AS effervescence, l.labsampnum, IIF(IsNULL(f.total_frags_pct), 0, f.total_frags_pct) AS total_frags_pct
 	FROM (
 	(
 	(
-	(
-	(
-	(pedon INNER JOIN phorizon ON pedon.peiid = phorizon.peiidref) 
+	pedon INNER JOIN phorizon ON pedon.peiid = phorizon.peiidref)
+
 	LEFT OUTER JOIN (SELECT phiidref, SUM(fragvol) as total_frags_pct FROM phfrags GROUP BY phiidref) as f on phorizon.phiid = f.phiidref)
+
 	LEFT OUTER JOIN (SELECT phiidref, labsampnum FROM phsample) as l ON phorizon.phiid = l.phiidref)
-	LEFT OUTER JOIN (SELECT phtiid, phiidref, texcl, lieutex FROM phtexture) as t ON phorizon.phiid = t.phiidref)
-	LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 189) AS tx ON t.texcl = tx.choice_id)
-	LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 192) AS til ON t.lieutex = til.choice_id)
+	
 	LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 1255) AS eff ON phorizon.effclass = eff.choice_id
+	
 	ORDER BY pedon.upedonid, phorizon.hzdept ASC;"
   
 	# setup connection to our pedon database
@@ -31,6 +35,27 @@ get_hz_data_from_pedon_db <- function(dsn) {
 	if(length(dupe.hz) > 0) {
 		cat(paste('notice: duplicate horizons in query results, matching pedons:\n', paste(unique(dupe.hz.pedon.ids), collapse=','), '\n', sep=''))
 	}
+	
+	
+	# next, get horizon textures and condence to 1:1 cardinality with results from above
+	q.texture <- "SELECT phorizon.phiid, IIF(IsNULL(tx.choice), til.choice, tx.choice) as texture_class
+FROM
+(
+(
+(
+phorizon
+LEFT OUTER JOIN (SELECT phtiid, phiidref, texcl, lieutex FROM phtexture) as t ON phorizon.phiid = t.phiidref)
+LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 189) AS tx ON t.texcl = tx.choice_id)
+LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 192) AS til ON t.lieutex = til.choice_id);"
+	
+	# exec query
+	d.texture <- sqlQuery(channel, q.texture, stringsAsFactors=FALSE)
+	
+	# concat multiple textures/horizon into a single record
+	d.texture <- aggregate(texture_class ~ phiid, data=d.texture, FUN=function(x) do.call(paste, as.list(x)))
+	
+	# join
+	d <- join(d, d.texture, by='phiid', type='left')
 	
 	# close connection
 	odbcClose(channel)
