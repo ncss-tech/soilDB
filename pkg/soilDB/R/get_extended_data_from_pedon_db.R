2a74abb3e150cfe59c2b5b352f83ba3979aefc10
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-21 21:41:48 +0000

    added jays updated query for splitting gr/pgr and cb/pgr

--- pkg/soilDB/R/get_extended_data_from_pedon_db.R
+++ pkg/soilDB/R/get_extended_data_from_pedon_db.R
@@ -6,38 +6,80 @@ FROM pediagfeatures
 	ORDER BY pediagfeatures.peiidref, pediagfeatures.featdept;"
 	
 	
-	##
-	## TODO: split GR/PGR and CB/PCB
-	##
 	# this query is resistant to dupes
 	# query rock-fragment summary by horizon
-	q.rf.summary <- "SELECT phfrags.phiidref as phiid, IIF(IsNULL(f1.gravel), 0.0, f1.gravel) as gravel, IIF(IsNULL(f2.cobbles), 0.0, f2.cobbles) as cobbles, IIF(IsNULL(f3.stones), 0.0, f3.stones) as stones, IIF(IsNULL(f4.boulders), 0.0, f4.boulders) as boulders, IIF(IsNULL(f5.channers), 0.0, f5.channers) as channers, IIF(IsNULL(f6.flagstones), 0.0, f6.flagstones) as flagstones
-FROM (((((((phorizon INNER JOIN phfrags ON phorizon.phiid = phfrags.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS gravel
+	q.rf.summary <- "SELECT DISTINCT phfrags.phiidref as phiid, 
+IIF(IsNULL(f1_gr.gravel), 0.0, f1_gr.gravel) as gravel, 
+IIF(IsNULL(f1_pgr.gravel), 0.0, f1_pgr.gravel) as paragravel, 
+IIF(IsNULL(f2_cb.cobbles), 0.0, f2_cb.cobbles) as cobbles, 
+IIF(IsNULL(f2_pcb.cobbles), 0.0, f2_pcb.cobbles) as paracobbles,
+IIF(IsNULL(f3.stones), 0.0, f3.stones) as stones, 
+IIF(IsNULL(f4.boulders), 0.0, f4.boulders) as boulders, 
+IIF(IsNULL(f5.channers), 0.0, f5.channers) as channers, 
+IIF(IsNULL(f6.flagstones), 0.0, f6.flagstones) as flagstones
+FROM ((((((((
+phfrags	
+LEFT OUTER JOIN (
+	SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS gravel
 	FROM phfrags
-	WHERE (((phfrags.fragsize_h)<=76) OR ((phfrags.fragsize_r)<=76 And (phfrags.fragsize_r)>=2))
-	GROUP BY phfrags.phiidref) as f1 ON phfrags.phiidref = f1.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS cobbles
-	FROM phfrags
-	WHERE (((phfrags.fragsize_l)>=75) AND ((phfrags.fragsize_h)<=250)) OR (((phfrags.fragsize_r)>=76 And (phfrags.fragsize_r)<=250))
-	GROUP BY phfrags.phiidref) as f2 ON phfrags.phiidref = f2.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS stones
-	FROM phfrags
-	WHERE (((phfrags.fragsize_l)>=250) OR ((phfrags.fragsize_r)>=250) OR (((phfrags.fragsize_l)>=380) AND (phfrags.fragshp)=1) OR   (((phfrags.fragsize_r)>=380) AND ((phfrags.fragshp)=1)))
-	GROUP BY phfrags.phiidref) as f3 ON phfrags.phiidref = f3.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS boulders
-	FROM phfrags
-	WHERE (((phfrags.fragsize_l)>=600) OR (((phfrags.fragsize_r)>=600)  AND ((phfrags.fragshp)=1)))
-	GROUP BY phfrags.phiidref) as f4 ON phfrags.phiidref = f4.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS channers
-	FROM phfrags
-	WHERE (((phfrags.fragsize_l)>=2) AND ((phfrags.fragsize_h)<=150) AND ((phfrags.fragshp)=1)) OR (((phfrags.fragshp)=1) AND ((phfrags.fragsize_r)>=2 And (phfrags.fragsize_r)<=150))
-	GROUP BY phfrags.phiidref) as f5 ON phfrags.phiidref = f5.phiidref)
-	LEFT OUTER JOIN (SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS flagstones
-	FROM phfrags
-	WHERE (((phfrags.fragsize_l)>=150) AND ((phfrags.fragsize_h)<=380) AND ((phfrags.fragshp)=1)) OR (((phfrags.fragshp)=1) AND   ((phfrags.fragsize_r)>=150 And (phfrags.fragsize_r)<=380))
-	GROUP BY phfrags.phiidref) as f6 ON phfrags.phiidref = f6.phiidref)
-	GROUP BY phfrags.phiidref, gravel, cobbles, stones, boulders, channers, flagstones
+LEFT OUTER JOIN (
+		SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE ((phfrags.fragsize_h <= 76) OR (phfrags.fragsize_r <= 76 And phfrags.fragsize_r >= 2))
+		AND (m.choice IN ('strongly', 'very strongly', 'indurated') OR m.choice IS NULL)
+		GROUP BY phfrags.phiidref
+	) as f1_gr ON phfrags.phiidref = f1_gr.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS gravel
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE (phfrags.fragsize_h <= 76 OR (phfrags.fragsize_r <= 76 And phfrags.fragsize_r >= 2))
+		AND m.choice NOT IN ('strongly', 'very strongly', 'indurated')
+		GROUP BY phfrags.phiidref
+	) as f1_pgr ON phfrags.phiidref = f1_pgr.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS cobbles
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE ((phfrags.fragsize_l >= 75 AND phfrags.fragsize_h <= 250) OR (phfrags.fragsize_r >= 76 And phfrags.fragsize_r <= 250))
+		AND (m.choice IN ('strongly', 'very strongly', 'indurated') OR m.choice IS NULL)
+		GROUP BY phfrags.phiidref
+	) as f2_cb ON phfrags.phiidref = f2_cb.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS cobbles
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE ((phfrags.fragsize_l >= 75 AND phfrags.fragsize_h <= 250) OR (phfrags.fragsize_r >= 76 And phfrags.fragsize_r <= 250))
+		AND m.choice NOT IN ('strongly', 'very strongly', 'indurated')
+		GROUP BY phfrags.phiidref
+	) as f2_pcb ON phfrags.phiidref = f2_pcb.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS stones
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE (((phfrags.fragsize_l)>=250) OR ((phfrags.fragsize_r)>=250) OR (((phfrags.fragsize_l)>=380) AND (phfrags.fragshp)=1) OR   		(((phfrags.fragsize_r)>=380) AND ((phfrags.fragshp)=1)))
+		GROUP BY phfrags.phiidref
+	) as f3 ON phfrags.phiidref = f3.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS boulders
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE (((phfrags.fragsize_l)>=600) OR (((phfrags.fragsize_r)>=600)  AND ((phfrags.fragshp)=1)))
+		GROUP BY phfrags.phiidref
+	) as f4 ON phfrags.phiidref = f4.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS channers
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE (((phfrags.fragsize_l)>=2) AND ((phfrags.fragsize_h)<=150) AND ((phfrags.fragshp)=1)) OR (((phfrags.fragshp)=1) AND   ((phfrags.fragsize_r)>=2 And (phfrags.fragsize_r)<=150))
+		GROUP BY phfrags.phiidref
+	) as f5 ON phfrags.phiidref = f5.phiidref)
+LEFT OUTER JOIN (
+		SELECT phfrags.phiidref, Sum(phfrags.fragvol) AS flagstones
+		FROM phfrags
+		LEFT OUTER JOIN (SELECT * FROM metadata_domain_detail WHERE metadata_domain_detail.domain_id = 173) AS m ON phfrags.fraghard = m.choice_id
+		WHERE (((phfrags.fragsize_l)>=150) AND ((phfrags.fragsize_h)<=380) AND ((phfrags.fragshp)=1)) OR (((phfrags.fragshp)=1) AND   ((phfrags.fragsize_r)>=150 And (phfrags.fragsize_r)<=380))
+		GROUP BY phfrags.phiidref
+	) as f6 ON phfrags.phiidref = f6.phiidref)	
 	ORDER BY phfrags.phiidref;"
 
 	
