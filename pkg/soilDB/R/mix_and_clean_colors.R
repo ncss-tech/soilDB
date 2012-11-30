2e633a02a401531b34ecb9668b45f5c6799bee3c
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-11-30 22:26:30 +0000

    added munsell value to data returned from nasis/pedonpc query functions

--- pkg/soilDB/R/mix_and_clean_colors.R
+++ pkg/soilDB/R/mix_and_clean_colors.R
@@ -8,11 +8,14 @@ mix_and_clean_colors <- function(x) {
 		r <- with(x, wtd.mean(r, weights=pct))
 		g <- with(x, wtd.mean(g, weights=pct))
 		b <- with(x, wtd.mean(b, weights=pct))
+		v <- with(x, wtd.mean(colorvalue, weights=pct))
 		# composite
-		df <- data.frame(r,g,b)
+		df <- data.frame(r=r, g=g, b=b, colorvalue=v)
 	}
+	
+	# note that if there is no mixing, then we have to reference the Munsell value by 'colorvalue'
 	else
-		df <- x[, c('r','g','b')]
+		df <- x[, c('r', 'g', 'b', 'colorvalue')]
 	
 	# done
 	return(df)
