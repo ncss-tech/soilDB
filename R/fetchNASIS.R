6728b733dd1dff4ab76700c33d592f8f49bdacab
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2013-04-04 22:18:47 +0000

    added fetchKSSL experimental function

--- R/fetchNASIS.R
+++ R/fetchNASIS.R
@@ -1,11 +1,9 @@
-# updated to NASIS 6.2 -- needs testing
-# horizon checking may be too strict
+# updated to NASIS 6.2
 
-## TODO: simplify and optimize using code from RCA functions
 # convenience function for loading most commonly used information from local NASIS database
 fetchNASIS <- function() {
 	
-	# 0. test connection
+	# test connection
 	if(! 'nasis_local' %in% names(odbcDataSources()))
 			stop('Local NASIS ODBC connection has not been setup. Please see the `setup_ODBC_local_NASIS.pdf` document included with this package.')
 	
@@ -15,80 +13,84 @@ fetchNASIS <- function() {
 	color_data <- get_colors_from_NASIS_db()
 	extended_data <- get_extended_data_from_NASIS_db()
 	
-	# test for multiple pedons / site
-	# note that this is also checked and reported on in get_site_data_from_NASIS_db()
-	multiple.pedons.per.site <- names(which(table(site_data$pedon_id) > 1))
-	
-	# 2. join pieces
-	# horizon + hz color: all horizons
+	# join horizon + hz color: all horizons
 	h <- join(hz_data, color_data, by='phiid', type='left')
 	
-	## TODO: this creates 2 columns of 'pedon_id', temp work-around is to remove it from site data
-	# (hz + color) + site: only those with horizon data
-	f <- join(h, site_data[, -which(names(site_data) == 'pedon_id')], by='peiid', type='inner')
-	
-	# 3. fix some common problems
+	# fix some common problems
 	# replace missing lower boundaries
 	message('replacing missing lower boundaries ...')
-	missing.lower.depth.idx <- which(!is.na(f$hzdept) & is.na(f$hzdepb))
-	f$hzdepb[missing.lower.depth.idx] <- f$hzdept[missing.lower.depth.idx]
+	missing.lower.depth.idx <- which(!is.na(h$hzdept) & is.na(h$hzdepb))
+	h$hzdepb[missing.lower.depth.idx] <- h$hzdept[missing.lower.depth.idx]
 	
-	## TODO: is this really a good idea?
-	# when multiple pedons / site, replace pedon_id with pedon_id-peiid
-	pedon.ids.to.fix.idx <- which(f$pedon_id %in% multiple.pedons.per.site)
-	if(length(pedon.ids.to.fix.idx) > 0)
+	# in the presence of duplicate pedons, replace pedon_id with pedon_id-peiid
+	# test for duplicate pedons
+	if(exists('dup.pedon.ids', envir=soilDB.env)) {
 		message('appending peiid to duplicate pedon IDs...')
+		
+		# extract from error reporting environment
+		dupe.pedons <- get('dup.pedon.ids', envir=soilDB.env)	
+		# convert into row-index
+		pedon.ids.to.fix.idx <- which(h$pedon_id %in% dupe.pedons)
+		
+		# fix offending pedon IDs by concatenating pedon_id with peiid
+		h$pedon_id[pedon.ids.to.fix.idx] <- paste(h$pedon_id[pedon.ids.to.fix.idx], h$peiid[pedon.ids.to.fix.idx], sep='-')
+	}
 	
-	# fix offending pedon IDs by concatenating pedon_id with peiid
-	f$pedon_id[pedon.ids.to.fix.idx] <- paste(f$pedon_id[pedon.ids.to.fix.idx], f$peiid[pedon.ids.to.fix.idx], sep='-')
+	# convert colors... in the presence of missing color data
+	h$soil_color <- NA
+	idx <- complete.cases(h$m_r)
+	h$soil_color[idx] <- with(h[idx, ], rgb(m_r, m_g, m_b)) # moist colors
 	
-	## TODO: this can be made more efficient
+	
+	# join hz + fragment summary
+	h <- join(h, extended_data$frag_summary, by='phiid', type='left')
+	
 	# test for bad horizonation... flag, and remove
 	message('finding horizonation errors ...')
-	f.test <- ddply(f, 'pedon_id', test_hz_logic, topcol='hzdept', bottomcol='hzdepb', strict=TRUE)
+	h.test <- ddply(h, 'pedon_id', test_hz_logic, topcol='hzdept', bottomcol='hzdepb', strict=TRUE)
 	
 	# which are the good (valid) ones?
-	good.pedon.ids <- as.character(f.test$pedon_id[which(f.test$hz_logic_pass)])
-	bad.pedon.ids <- as.character(f.test$pedon_id[which(f.test$hz_logic_pass == FALSE)])
+	good.pedon.ids <- as.character(h.test$pedon_id[which(h.test$hz_logic_pass)])
+	bad.pedon.ids <- as.character(h.test$pedon_id[which(h.test$hz_logic_pass == FALSE)])
 	
 	# keep the good ones
-	f <- subset(f, pedon_id %in% good.pedon.ids)
+	h <- subset(h, pedon_id %in% good.pedon.ids)
 	
-	# 4. upgrade to SoilProfilecollection
-	depths(f) <- pedon_id ~ hzdept + hzdepb
+	# upgrade to SoilProfilecollection
+	depths(h) <- pedon_id ~ hzdept + hzdepb
 	
-	# move site data into @site
-	site(f) <- ~ peiid + site_id + siteiid + hillslope_pos + slope_position + shapeacross + shapedown + slopecomplex + drainagecl + x + y + datum + x_std + y_std + elev_field + slope_field + aspect_field + plantassocnm + bedrckdepth + bedrock_kind + bedrock_hardness + describer + psctopdepth + pscbotdepth + obs_date + pedon_purpose + pedon_type + pedlabsampnum
+	## TODO: this is slow
+	# move peiid into @site, this will be used to join full table of site data
+	site(h) <- ~ peiid
 	
-	# 5. convert colors... in the presence of missing color data
-	f$soil_color <- rep(NA, times=nrow(horizons(f)))
-	idx <- complete.cases(f$m_r)
-	f$soil_color[idx] <- with(horizons(f)[idx, ], rgb(m_r, m_g, m_b)) # moist colors
+	## TODO: this will fail in the presence of duplicates
+	# add site data to object
+	site_data$pedon_id <- NULL # remove 'pedon_id' column from site_data
+	site(h) <- site_data # left-join via peiid
 	
-	# 6. merge-in extended data:
-	# replace horizons with hz + fragment summary
-	horizons(f) <- join(horizons(f), extended_data$frag_summary, by='phiid', type='left')
 	
-	# add diagnostic boolean data into @site
-	site(f) <- extended_data$diagHzBoolean
-	
-	# add surface frag summary
-	site(f) <- extended_data$surf_frag_summary
-	
-	# load diagnostic horizons into @diagnostic: note that this requires one additional join 
-	# and implicitly filters diagnostic hz by our subset of f
-	diagnostic_hz(f) <- join(site(f)[, c('pedon_id','peiid')], extended_data$diagnostic, by='peiid', type='left')
-	
-	# 6.1 load best-guess optimal records from taxhistory
+	### TODO: consider moving this into the extended data function ###
+	# load best-guess optimal records from taxhistory
 	# method is added to the new field called 'selection_method'
 	best.tax.data <- ddply(extended_data$taxhistory, 'peiid', pickBestTaxHistory)
-	site(f) <- best.tax.data
+	site(h) <- best.tax.data
 	
+	# add diagnostic boolean data into @site
+	site(h) <- extended_data$diagHzBoolean
+	
+	# add surface frag summary
+	site(h) <- extended_data$surf_frag_summary
+	
+	## TODO: we don't really need pedon_id in @diagnostic
+	# load diagnostic horizons into @diagnostic: note that this requires one additional join 
+	# and implicitly filters diagnostic hz by our subset of f
+	diagnostic_hz(h) <- join(site(h)[, c('pedon_id','peiid')], extended_data$diagnostic, by='peiid', type='left')
+		
 	# 7. save and mention bad pedons
 	assign('bad.pedon.ids', value=bad.pedon.ids, envir=soilDB.env)
 	if(length(bad.pedon.ids) > 0)
 		message("horizon errors detected, use `get('bad.pedon.ids', envir=soilDB.env)` for a list of pedon IDs")
 	
 	# done
-	return(f)
+	return(h)
 }
