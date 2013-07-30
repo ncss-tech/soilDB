020cf405fc2086c99cbb8710f068bd58e2d780d5
dylan <dylan@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-07-30 18:09:26 +0000

    fetchRaCA() now pulls data as a gzip stream from the SoilWeb sever. VNIR spectra are optionally included

--- pkg/soilDB/R/fetchRaCA.R
+++ pkg/soilDB/R/fetchRaCA.R
@@ -1,10 +1,11 @@
 ## TODO: need to implement some kind of constraints on geographic queries
-## TODO: consider compressing the data stream
-## TODO: use download.file() to get gzipped stream from server
-## TODO: consider compression at the www server: http://httpd.apache.org/docs/2.0/mod/mod_deflate.html
+## NOTE: each VNIR spectra record is 6.6kb of data (compressed, gzip, level 5)
 
 # this loads and packages the data into a list of objects
-fetchRaCA <- function(series=NULL, bbox=NULL) {
+fetchRaCA <- function(series=NULL, bbox=NULL, get.vnir=FALSE) {
+  
+  # important: change the default behavior of data.frame and melt
+  opt.original <- options(stringsAsFactors = FALSE)
   
   # sanity-check: user must supply some kind of criteria
   if(missing(series) & missing(bbox))
@@ -20,6 +21,8 @@ fetchRaCA <- function(series=NULL, bbox=NULL) {
   veg <- NULL
   conc <- NULL
   stock <- NULL
+  vnir <- NULL
+  spectra <- NULL
   
   # process filter components
   if(!missing(series)) {
@@ -35,21 +38,60 @@ fetchRaCA <- function(series=NULL, bbox=NULL) {
   f <- paste(f, collapse='')
   
   # build URLs
-  site.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=site', f, sep=''))
-  hz.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=horizon', f, sep=''))
-  trees.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=trees', f, sep=''))
-  veg.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=veg', f, sep=''))
-  conc.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=conc', f, sep=''))
-  stock.url <- url(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=stock', f, sep=''))
+  site.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=site', f, sep=''))
+  hz.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=horizon', f, sep=''))
+  trees.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=trees', f, sep=''))
+  veg.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=veg', f, sep=''))
+  conc.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=conc', f, sep=''))
+  stock.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=stock', f, sep=''))
+  vnir.url <- URLencode(paste('http://casoilresource.lawr.ucdavis.edu/soil_web/rca/rca_query.php?what=vnir', f, sep=''))
   
+  # init temp files
+  tf.site <- tempfile()
+  tf.hz <- tempfile()
+  tf.trees <- tempfile()
+  tf.veg <- tempfile()
+  tf.conc <- tempfile()
+  tf.stock <- tempfile()
+  tf.vnir <- tempfile()
+  
+  # download pieces
+  download.file(url=site.url, destfile=tf.site, mode='wb', quiet=TRUE)
+  download.file(url=hz.url, destfile=tf.hz, mode='wb', quiet=TRUE)
+  download.file(url=trees.url, destfile=tf.trees, mode='wb', quiet=TRUE)
+  download.file(url=veg.url, destfile=tf.veg, mode='wb', quiet=TRUE)
+  download.file(url=conc.url, destfile=tf.conc, mode='wb', quiet=TRUE)
+  download.file(url=stock.url, destfile=tf.stock, mode='wb', quiet=TRUE)
   
   # load pieces
-  try(s <- read.table(site.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
-  try(h <- read.table(hz.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
-  try(trees <- read.table(trees.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
-  try(veg <- read.table(veg.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
-  try(conc <- read.table(conc.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
-  try(stock <- read.table(stock.url, header=TRUE, sep='|', stringsAsFactors=FALSE), silent=TRUE)
+  try(s <- read.table(gzfile(tf.site), header=TRUE, sep='|'), silent=TRUE)
+  try(h <- read.table(gzfile(tf.hz), header=TRUE, sep='|'), silent=TRUE)
+  try(trees <- read.table(gzfile(tf.trees), header=TRUE, sep='|'), silent=TRUE)
+  try(veg <- read.table(gzfile(tf.veg), header=TRUE, sep='|'), silent=TRUE)
+  try(conc <- read.table(gzfile(tf.conc), header=TRUE, sep='|'), silent=TRUE)
+  try(stock <- read.table(gzfile(tf.stock), header=TRUE, sep='|'), silent=TRUE)
+  
+  # optionally load spectra
+  if(get.vnir) {
+    message('spectra are large, download may take some time...', appendLF=TRUE)
+    
+    # save the file locally
+    download.file(url=vnir.url, destfile=tf.vnir, mode='wb', quiet=FALSE)
+    # try to open
+    try(vnir <- read.table(gzfile(tf.vnir), header=TRUE, sep='|'), silent=TRUE)
+    
+    # test for missing data
+    if(!is.null(vnir)) {
+      # extract and parse the serialized spectra as matrix
+      spectra <- as.matrix(read.table(textConnection(vnir$spectra), header=FALSE, sep=','))
+      
+      # since order is preserved (row-wise and col-wise), we can assign:
+      # rownames = sample_id
+      # colnames = wavenumber
+      dimnames(spectra)[[1]] <- vnir$sample_id
+      dimnames(spectra)[[2]] <- 350:2500
+    }
+  }
   
   # report missing data
   if(all(c(is.null(s), is.null(h)))) {
@@ -64,6 +106,10 @@ fetchRaCA <- function(series=NULL, bbox=NULL) {
   # coordinates(h) <- ~ x + y
   # proj4string(h) <- '+proj=longlat +datum=WGS84'
   
+  # reset options:
+  options(opt.original)
+  
   # pack into a list and return
-  return(list(pedons=h, trees=trees, veg=veg, conc=conc, stock=stock))
+  return(list(pedons=h, trees=trees, veg=veg, conc=conc, stock=stock, spectra=spectra))
+  
 }
