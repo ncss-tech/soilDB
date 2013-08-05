c04679bffacf0a868905fa8ae0b385282d9c08d0
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2013-08-05 20:04:41 +0000

    added US State queries via two letter state abbreviation

--- R/fetchRaCA.R
+++ R/fetchRaCA.R
@@ -2,13 +2,13 @@
 ## NOTE: each VNIR spectra record is 6.6kb of data (compressed, gzip, level 5)
 
 # this loads and packages the data into a list of objects
-fetchRaCA <- function(series=NULL, bbox=NULL, get.vnir=FALSE) {
+fetchRaCA <- function(series=NULL, bbox=NULL, state=NULL, get.vnir=FALSE) {
   
   # important: change the default behavior of data.frame
   opt.original <- options(stringsAsFactors = FALSE)
   
   # sanity-check: user must supply some kind of criteria
-  if(missing(series) & missing(bbox))
+  if(missing(series) & missing(state) & missing(bbox))
     stop('you must provide some filtering criteria')
   
   # init empty filter
@@ -32,6 +32,10 @@ fetchRaCA <- function(series=NULL, bbox=NULL, get.vnir=FALSE) {
   if(!missing(bbox)) {
     bbox <- paste(bbox, collapse=',')
     f <- c(f, paste('&bbox=', bbox, sep=''))
+  }
+  
+  if(!missing(state)) {
+    f <- c(f, paste('&state=', state, sep=''))
   }
   
   # combine filters
