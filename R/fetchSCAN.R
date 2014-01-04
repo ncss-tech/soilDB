4cc0a530899495b280c73637e3f32b4c2b9836ea
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2014-01-04 00:47:30 +0000

    fetchSCAN() now attempts to get column names from source data. also added some documentation

--- R/fetchSCAN.R
+++ R/fetchSCAN.R
@@ -24,11 +24,37 @@ fetchSCAN <- function(req) {
   # connect to the text as a standard file
   tc <- textConnection(res)
   
+  # attempt to read column headers, after skipping the first two lines of data
+  h <- unlist(read.table(tc, nrows=1, skip=2, header=FALSE, stringsAsFactors=FALSE, sep=',', quote='', strip.white=TRUE, na.strings='-99.9', comment.char=''))
+  
+  # the last header is junk (NA)
+  h <- as.vector(na.omit(h))
+  
+  # split colunmn names on white space and keep the first element
+  h <- sapply(strsplit(h, split=' '), function(i) i[[1]])
+  
+  # clean some more junk
+  h <- gsub('-1', '', fixed=TRUE, h)
+  h <- gsub(':-', '_', h)
+  
+  
+  # NOTE: we have already read-in the first 3 lines above, therefore we don't need to skip lines here
   # read as CSV, skipping junk + headers, accomodating white-space and NA values encoded as -99.9
-  x <- read.table(tc, skip=3, header=FALSE, stringsAsFactors=FALSE, sep=',', quote='', strip.white=TRUE, na.strings='-99.9', comment.char='')
+  x <- read.table(tc, header=FALSE, stringsAsFactors=FALSE, sep=',', quote='', strip.white=TRUE, na.strings='-99.9', comment.char='')
+  
+  # the last column is always junk
+  x[[names(x)[length(x)]]] <- NULL
+  
+  # apply truncated column names:
+  names(x) <- h
+  
+  # clean-up connections
+  close.connection(tc)
+  
+  # convert date to Date class
+  x$Date <- as.Date(x$Date)
   
   # done
-  close.connection(tc)
   return(x)
 }
 
