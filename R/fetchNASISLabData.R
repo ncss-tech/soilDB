57008e5c963c2c70581e8b9374f5b376ddbcf47f
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2014-03-11 22:36:50 +0000

    fixing initial versoin of fetchNASISLabData()

--- R/fetchNASISLabData.R
+++ R/fetchNASISLabData.R
@@ -7,12 +7,10 @@ fetchNASISLabData <- function() {
 	if(! 'nasis_local' %in% names(odbcDataSources()))
 			stop('Local NASIS ODBC connection has not been setup. Please see the `setup_ODBC_local_NASIS.pdf` document included with this package.')
 	
-	# 1. load data in pieces
+	# 1. load data in pieces, results are DF objects
 	s <- get_labpedon_data_from_NASIS_db()
 	h <- get_lablayer_data_from_NASIS_db()
 	
-	# convert layers from list to dataframe
-	h <- as.data.frame(h)
 		
 	# fix some common problems
 	# replace missing lower boundaries
@@ -23,38 +21,32 @@ fetchNASISLabData <- function() {
   }
 	
 	
-	# test for bad horizonation... flag, and remove
+  ## TODO: what to do with multiple samples / hz?
+	# test for bad horizonation... flag
 	message('finding horizonation errors ...')
 	h.test <- ddply(h, 'labpeiid', test_hz_logic, topcol='hzdept', bottomcol='hzdepb', strict=TRUE)
 	
 	# which are the good (valid) ones?
-	good.ids <- as.character(h.test$peiid[which(h.test$hz_logic_pass)])
-	bad.ids <- as.character(h.test$peiid[which(!h.test$hz_logic_pass)])
-  bad.pedon.ids <- site_data$pedon_id[which(site_data$peiid %in% bad.ids)]
+	good.ids <- as.character(h.test$labpeiid[which(h.test$hz_logic_pass)])
+	bad.ids <- as.character(h.test$labpeiid[which(!h.test$hz_logic_pass)])
+  bad.pedon.ids <- s$upedonid[which(s$labpeiid %in% bad.ids)]
 	
-	# keep the good ones
-	h <- h[which(h$peiid %in% good.ids), ]
+  # subset ?
+  
 	
 	# upgrade to SoilProfilecollection
 	depths(h) <- labpeiid ~ hzdept + hzdepb
 	
-	## TODO: this is slow
-	# move pedon_id into @site
-	site(h) <- ~ pedon_id
 	
 	## TODO: this will fail in the presence of duplicates
 	# add site data to object
-	site_data$pedon_id <- NULL # remove 'pedon_id' column from site_data
-	site(h) <- site_data # left-join via peiid
+	site(h) <- s # left-join via labpeiid
 	
-	
-	# add surface frag summary
-	site(h) <- extended_data$surf_frag_summary
-		
+  
 	# 7. save and mention bad pedons
-	assign('bad.pedon.ids', value=bad.pedon.ids, envir=soilDB.env)
+	assign('bad.labpedon.ids', value=bad.pedon.ids, envir=soilDB.env)
 	if(length(bad.pedon.ids) > 0)
-		message("horizon errors detected, use `get('bad.pedon.ids', envir=soilDB.env)` for a list of pedon IDs")
+		message("horizon errors detected, use `get('bad.labpedon.ids', envir=soilDB.env)` for a list of pedon IDs")
 	
 	# done
 	return(h)
