32bafb272ad102532cf4cf0620778656f35bc66f
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2015-11-02 19:12:04 +0000

    removing old SDA SSOAP/XMLSchema code and deps

--- R/SDA_query.R
+++ R/SDA_query.R
@@ -7,17 +7,7 @@ format_SQL_in_statement <- function(x) {
 	return(i)
 }
 
-# clean-up results from SDA SOAP query, and return as DF
-.cleanSDA <- function(i) {
-	# remove left-overs from SOAP result
-	i$.attrs <- NULL
-	
-	# convert NULL in NA
-	i[which(sapply(i, is.null))] <- NA
-	
-	# convert list to DF
-	return(as.data.frame(i, stringsAsFactors=FALSE))
-}
+
 
 ## TODO: requires more testing and error-trapping
 SDA_query <- function(q) {
@@ -38,7 +28,6 @@ SDA_query <- function(q) {
   cat(post.data, file=tf.1, sep = '\n')
   
   # submit request
-  cat('sending POST request...\n')
   r <- httr::POST(url="http://sdmdataaccess.sc.egov.usda.gov/tabular/post.rest", body=httr::upload_file(tf.1))
   httr::stop_for_status(r)
   
@@ -46,8 +35,16 @@ SDA_query <- function(q) {
   r.content <- httr::content(r)
   d <- xmlToDataFrame(r.content, stringsAsFactors = FALSE)
   
-  # first line and column are garbage
+  # first line is garbage
   d <- d[-1, ]
+  
+  # check for no returned data, 'd' will be a character object with 0 elements
+  if(class(d) == 'character') {
+    message('query returned 0 rows')
+    return(NULL)
+  }
+  
+  # first column is garbage
   d$element <- NULL
   
   # save to file / re-load to guess column classes
@@ -63,44 +60,59 @@ SDA_query <- function(q) {
   return(df)
 }
 
-.oldSDA_query <- function(q) {
-  # check for required packages
-  if(!requireNamespace('SSOAP', quietly=TRUE) | !requireNamespace('XMLSchema', quietly=TRUE))
-    stop('please install the `SSOAP` and `XMLSchema` packages', call.=FALSE)
-    
-  # important: change the default behavior of data.frame
-	opt.original <- options(stringsAsFactors = FALSE)
-	
-	# setup server, action, and xmlns
-	s <- SSOAP::SOAPServer('SDMDataAccess.nrcs.usda.gov', '/Tabular/SDMTabularService.asmx')
-	a <- I('http://SDMDataAccess.nrcs.usda.gov/Tabular/SDMTabularService.asmx/RunQuery')
-	x <- c(I("http://SDMDataAccess.nrcs.usda.gov/Tabular/SDMTabularService.asmx"))
-	
-	# feedback:
-	cat('sending SOAP request...\n')
-	
-	# submit and process the query
-	res <- SSOAP::.SOAP(s, "RunQuery", Query=q, action=a, xmlns=x)
-	
-	# results are stored in: res$diffgram$NewDataSet
-	
-	# clean the results, convert to DF
-	cat('processing results...\n')
-	
-	df <- ldply(res$diffgram$NewDataSet, .fun=.cleanSDA, .progress='text')
-	df$.id <- NULL
-	
-	# temp hack: everything is read-in as character data!!
-	# write out to tempfile, and read back in
-	f <- tempfile()
-	write.table(df, file=f, col.names=TRUE, row.names=FALSE, quote=FALSE, sep='|')
-	df <- read.table(f, header=TRUE, sep='|', quote='', comment.char='')
-	
-	# reset options:
-	options(opt.original)
-	
-	# done
-	return(df)
-}
+
+### this is the old-style interface, now depreciated ###
+# 
+# # clean-up results from SDA SOAP query, and return as DF
+# .cleanSDA <- function(i) {
+#   # remove left-overs from SOAP result
+#   i$.attrs <- NULL
+#   
+#   # convert NULL in NA
+#   i[which(sapply(i, is.null))] <- NA
+#   
+#   # convert list to DF
+#   return(as.data.frame(i, stringsAsFactors=FALSE))
+# }
+# 
+# .oldSDA_query <- function(q) {
+#   # check for required packages
+#   if(!requireNamespace('SSOAP', quietly=TRUE) | !requireNamespace('XMLSchema', quietly=TRUE))
+#     stop('please install the `SSOAP` and `XMLSchema` packages', call.=FALSE)
+#     
+#   # important: change the default behavior of data.frame
+# 	opt.original <- options(stringsAsFactors = FALSE)
+# 	
+# 	# setup server, action, and xmlns
+# 	s <- SSOAP::SOAPServer('SDMDataAccess.nrcs.usda.gov', '/Tabular/SDMTabularService.asmx')
+# 	a <- I('http://SDMDataAccess.nrcs.usda.gov/Tabular/SDMTabularService.asmx/RunQuery')
+# 	x <- c(I("http://SDMDataAccess.nrcs.usda.gov/Tabular/SDMTabularService.asmx"))
+# 	
+# 	# feedback:
+# 	cat('sending SOAP request...\n')
+# 	
+# 	# submit and process the query
+# 	res <- SSOAP::.SOAP(s, "RunQuery", Query=q, action=a, xmlns=x)
+# 	
+# 	# results are stored in: res$diffgram$NewDataSet
+# 	
+# 	# clean the results, convert to DF
+# 	cat('processing results...\n')
+# 	
+# 	df <- ldply(res$diffgram$NewDataSet, .fun=.cleanSDA, .progress='text')
+# 	df$.id <- NULL
+# 	
+# 	# temp hack: everything is read-in as character data!!
+# 	# write out to tempfile, and read back in
+# 	f <- tempfile()
+# 	write.table(df, file=f, col.names=TRUE, row.names=FALSE, quote=FALSE, sep='|')
+# 	df <- read.table(f, header=TRUE, sep='|', quote='', comment.char='')
+# 	
+# 	# reset options:
+# 	options(opt.original)
+# 	
+# 	# done
+# 	return(df)
+# }
 
 
