15c86ebc29122d921630829469f2d12f4372d15d
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-10-06 13:00:02 -0700

    adding basic SCAN metadata getter

--- R/fetchSCAN.R
+++ R/fetchSCAN.R
@@ -7,11 +7,59 @@
 ## see: http://www.wcc.nrcs.usda.gov/web_service/awdb_webservice_announcements.htm
 ##      http://www.wcc.nrcs.usda.gov/web_service/AWDB_Web_Service_Reference.htm
 ##      http://www.wcc.nrcs.usda.gov/report_generator/WebReportScripting.htm
+#
+### sensor codes: http://wcc.sc.egov.usda.gov/nwcc/sensors
+
 
 ##
 ## ideas:
 ##   https://github.com/gunnarleffler/getSnotel
 ##
+
+
+SCAN_sensor_metadata <- function(site.code) {
+  # check for required packages
+  if(!requireNamespace('httr', quietly = TRUE) | !requireNamespace('rvest', quietly = TRUE))
+    stop('please install the `httr` and `rvest` packages', call.=FALSE)
+  
+  # base URL to service
+  uri <- 'http://wcc.sc.egov.usda.gov/nwcc/sensors'
+  
+  # note: the SCAN form processor checks the refering page and user-agent
+  new.headers <- c(
+    "Referer"="http://www.wcc.nrcs.usda.gov/nwcc/",
+    "User-Agent" = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13"
+  )
+  
+  # enable follow-location
+  # http://stackoverflow.com/questions/25538957/suppressing-302-error-returned-by-httr-post
+  # cf <- httr::config(followlocation = 1L, verbose=1L) # debugging
+  cf <- httr::config(followlocation = 1L)
+  
+  req <- list(sitenum=site.code, report='ALL', interval='DAY', timeseries=" View Daily Sensor Descriptions ")
+  
+  # submit request
+  r <- httr::POST(uri, body=req, encode='form', config = cf, httr::add_headers(new.headers))
+  httr::stop_for_status(r)
+  
+  # parsed XML
+  r.content <- httr::content(r, as='parsed')
+  # get tables
+  n.tables <- rvest::html_nodes(r.content, "table")
+  
+  # the metadata table we want is 5th
+  m <- rvest::html_table(n.tables[[5]], header=FALSE)
+  
+  # clean-up table
+  # 1st row is header
+  h <- make.names(m[1, ])
+  # second row is junk
+  m <- m[-c(1:2), ]
+  names(m) <- h
+  
+  return(m)
+}
+
 
 
 # req is a named vector or list
@@ -82,62 +130,6 @@ fetchSCAN <- function(req) {
   return(x)
 }
 
-
-### old version based on RCurl
-# fetchSCAN.old <- function(req) {
-#   
-#   # check for required packages
-#   if(!requireNamespace('httr', quietly = TRUE))
-#     stop('please install the `httr` package', call.=FALSE)
-#   
-#   # base URL to service
-#   uri <- 'http://www.wcc.nrcs.usda.gov/nwcc/view'
-#   
-#   # set CURL options here
-#   o <- curlOptions(
-#     referer="http://www.wcc.nrcs.usda.gov/nwcc/",
-#     useragent = "Mozilla/5.0 (Macintosh; U; Intel Mac OS X 10.6; en-US; rv:1.9.2.13) Gecko/20101203 Firefox/3.6.13"
-#   )
-#   
-#   # post form, result is a pile of text
-#   res <- postForm(uri, .params=req, .opts=o, style="POST")
-#   
-#   # connect to the text as a standard file
-#   tc <- textConnection(res)
-#   
-#   # attempt to read column headers, after skipping the first two lines of data
-#   h <- unlist(read.table(tc, nrows=1, skip=2, header=FALSE, stringsAsFactors=FALSE, sep=',', quote='', strip.white=TRUE, na.strings='-99.9', comment.char=''))
-#   
-#   # the last header is junk (NA)
-#   h <- as.vector(na.omit(h))
-#   
-#   # split colunmn names on white space and keep the first element
-#   h <- sapply(strsplit(h, split=' '), function(i) i[[1]])
-#   
-#   # clean some more junk
-#   h <- gsub('-1', '', fixed=TRUE, h)
-#   h <- gsub(':-', '_', h)
-#   
-#   
-#   # NOTE: we have already read-in the first 3 lines above, therefore we don't need to skip lines here
-#   # read as CSV, skipping junk + headers, accomodating white-space and NA values encoded as -99.9
-#   x <- read.table(tc, header=FALSE, stringsAsFactors=FALSE, sep=',', quote='', strip.white=TRUE, na.strings='-99.9', comment.char='')
-#   
-#   # the last column is always junk
-#   x[[names(x)[length(x)]]] <- NULL
-#   
-#   # apply truncated column names:
-#   names(x) <- h
-#   
-#   # clean-up connections
-#   close.connection(tc)
-#   
-#   # convert date to Date class
-#   x$Date <- as.Date(x$Date)
-#   
-#   # done
-#   return(x)
-# }
 
 
 
