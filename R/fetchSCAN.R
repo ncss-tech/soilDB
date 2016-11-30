beeb3e4790111d5e30ffceae37f571a028ba00f2
Dylan Beaudette <dylan.beaudette@gmail.com>
2016-11-30 15:40:27 -0800

    adding link to GH issue

--- R/fetchSCAN.R
+++ R/fetchSCAN.R
@@ -145,6 +145,7 @@ fetchSCAN <- function(site.code, year, report='SCAN', req=NULL) {
   if(length(d.cols) == 0)
     return(NULL)
   
+  ## https://github.com/ncss-tech/soilDB/issues/14
   ## temporary hack to inform users that there are multiple sensors / label
   if(length(d.cols) > 1) {
     message(paste0('WARNING multiple sensors per site: ', d$Site[1], ' [', paste0(names(d)[d.cols], collapse = ','), '], using first sensor'))
