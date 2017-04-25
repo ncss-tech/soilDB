aeb9b5a19c26221c6e26855e4a8b6803568dc47c
Stephen Roecker <stephen.roecker@gmail.com>
2017-04-25 14:14:48 -0400

    adding uncode() function

---
+++ R/uncode.R
@@ -0,0 +1,68 @@
+uncode <- function(df, invert=FALSE, NASIS=TRUE){
+  get_metadata <- function() {
+    # must have RODBC installed
+    if(!requireNamespace('RODBC'))
+      stop('please install the `RODBC` package', call.=FALSE)
+    
+    q <- "SELECT mdd.DomainID, DomainName, ChoiceValue, ChoiceLabel, ChoiceDescription, ColumnPhysicalName, ColumnLogicalName
+    
+    FROM MetadataDomainDetail mdd
+    INNER JOIN MetadataDomainMaster mdm ON mdm.DomainID = mdd.DomainID
+    INNER JOIN (SELECT MIN(DomainID) DomainID, MIN(ColumnPhysicalName) ColumnPhysicalName, MIN(ColumnLogicalName) ColumnLogicalName FROM MetadataTableColumn GROUP BY DomainID, ColumnPhysicalName) mtc ON mtc.DomainID = mdd.DomainID
+    
+    ORDER BY DomainID, ColumnPhysicalName, ChoiceValue;"
+    
+    # setup connection local NASIS
+    channel <- RODBC::odbcDriverConnect(connection = "DSN=nasis_local; UID=NasisSqlRO; PWD=nasisRe@d0n1y")
+    
+    # exec query
+    d <- RODBC::sqlQuery(channel, q, stringsAsFactors = FALSE)
+    
+    # close connection
+    RODBC::odbcClose(channel)
+    
+    # done
+    return(d)
+  }
+  
+  # load current metadata table
+  if (NASIS == TRUE){
+    metadata <- get_metadata()
+  } else data(nasis_metadata)
+  
+  # unique set of possible columns that will need replacement
+  possibleReplacements <- unique(metadata$ColumnPhysicalName)
+  
+  # names of raw data
+  nm <- names(df)
+  # index to columns with codes to be replaced
+  columnsToWorkOn.idx <- which(nm %in% possibleReplacements)
+  
+  # iterate over columns with codes
+  for (i in columnsToWorkOn.idx){
+    # get the current metadata
+    sub <- metadata[metadata$ColumnPhysicalName %in% nm[i], ]
+    # NASIS == TRUE
+    if (NASIS == TRUE) {
+      if (invert == FALSE){
+        # replace codes with values
+        df[, i] <- factor(df[, i], levels = sub$ChoiceValue, labels = sub$ChoiceLabel)
+      } else { 
+        # replace values with codes
+        df[, i] <- factor(df[, i], levels = sub$ChoiceLabel, labels = sub$ChoiceValue)}
+    }
+    # NASIS == FALSE
+    if (NASIS == FALSE) {
+      if (invert == FALSE){
+        # replace codes with values
+        df[, i] <- factor(df[, i], levels = sub$ChoiceValue, labels = sub$ChoiceLabel)
+      } else {
+        # replace values with codes
+        df[, i] <- factor(df[, i], levels = sub$ChoiceLabel, labels = sub$ChoiceValue)}
+      }
+    }
+  
+  return(df)
+}
+
+code <- function(df, invert = TRUE, NASIS = TRUE) {uncode(df, invert, NASIS)}
