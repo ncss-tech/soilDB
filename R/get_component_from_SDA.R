640b2f773325645680c42e9ee784c65434f442a5
Stephen Roecker <stephen.roecker@gmail.com>
2017-05-02 10:40:04 -0400

    fixing the duplicates argument for get_component_from_SDA.R

--- R/get_component_from_SDA.R
+++ R/get_component_from_SDA.R
@@ -1,11 +1,15 @@
-get_component_from_SDA <- function(WHERE = NULL, duplicates){
+get_component_from_SDA <- function(WHERE = NULL, duplicates = FALSE){
   # SDA is missing soiltempa_r AS mast_r
   # Joining in the fetch on derived_cokey doesn't work but should. There are duplicate components with the same combination of elements.
   # paste0("mu.nationalmusym + '_' + CAST(comppct_r AS VARCHAR) + '_' + compname + '-' + ISNULL(localphase, 'no_phase') AS derived_cokey")
   q.component <- paste("SELECT mu.nationalmusym, compname, comppct_r, compkind, majcompflag, localphase, slope_r, tfact, wei, weg, drainagecl, elev_r, aspectrep, map_r, airtempa_r AS maat_r, reannualprecip_r, ffd_r, nirrcapcl, nirrcapscl, irrcapcl, irrcapscl, frostact, hydgrp, corcon, corsteel, taxclname, taxorder, taxsuborder, taxgrtgroup, taxsubgrp, taxpartsize, taxpartsizemod, taxceactcl, taxreaction, taxtempcl, taxmoistscl, taxtempregime, soiltaxedition, cokey
       
-  FROM legend l INNER JOIN
-       mapunit mu ON mu.lkey = l.lkey INNER JOIN
+  FROM legend l INNER JOIN",
+       if (duplicates == FALSE) {
+         paste0("(SELECT MIN(nationalmusym) nationalmusym, MIN(MUKEY) mukey, MIN(lkey) lkey
+        FROM mapunit
+        GROUP BY nationalmusym) AS mu")
+         } else "mapunit mu", "ON mu.lkey = l.lkey INNER JOIN
        component c ON c.mukey = mu.mukey
           
   WHERE", WHERE,
@@ -66,8 +70,12 @@ get_chorizon_from_SDA <- function(WHERE = NULL, duplicates = FALSE) {
   # seriously!, how is their no fragvoltot_r column?
   q.chorizon <- paste("SELECT hzname, hzdept_r, hzdepb_r, sandtotal_l, sandtotal_r, sandtotal_h, silttotal_l, silttotal_r, silttotal_h, claytotal_l, claytotal_r, claytotal_h, texture, om_l, om_r, om_h, dbovendry_r, ksat_r, awc_l, awc_r, awc_h, lep_r, sar_r, ec_r, cec7_r, sumbases_r, ph1to1h2o_l, ph1to1h2o_r, ph1to1h2o_h, c.cokey
   
-  FROM legend l INNER JOIN
-       mapunit mu ON mu.lkey = l.lkey INNER JOIN
+  FROM legend l INNER JOIN",
+       if (duplicates == FALSE) {
+          paste0("(SELECT MIN(nationalmusym) nationalmusym, MIN(MUKEY) mukey, MIN(lkey) lkey
+                   FROM mapunit
+                   GROUP BY nationalmusym) AS mu")
+          } else "mapunit mu", "ON mu.lkey = l.lkey INNER JOIN
        component c ON c.mukey = mu.mukey INNER JOIN
        chorizon ch ON ch.cokey = c.cokey LEFT OUTER JOIN
        chtexturegrp chtg ON chtg.chkey = ch.chkey AND rvindicator = 'YES'
