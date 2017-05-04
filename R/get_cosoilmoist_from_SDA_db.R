36c3c2dc16198b460a2a072f38fecab5cb6cba0f
Stephen Roecker <stephen.roecker@gmail.com>
2017-05-04 16:06:00 -0400

    removed the mukey argument from get_cosoilmoist_from_SDA_db.R and replaced with WHERE

--- R/get_cosoilmoist_from_SDA_db.R
+++ R/get_cosoilmoist_from_SDA_db.R
@@ -1,14 +1,23 @@
-get_cosoilmoist_from_SDA_db <- function(mukey, impute = TRUE){
-  q.cosoilmoist <- paste0("SELECT mu.nationalmusym, mu.mukey, mu.muname, c.compname, c.comppct_r, month, flodfreqcl, pondfreqcl, soimoistdept_l, soimoistdept_r, soimoistdept_h, soimoistdepb_l, soimoistdepb_r, soimoistdepb_h, soimoiststat
+get_cosoilmoist_from_SDA_db <- function(WHERE = NULL, duplicates = FALSE, impute = TRUE){
+  q.cosoilmoist <- paste("SELECT", 
+  if (duplicates == FALSE) {"DISTINCT"}
+, "nationalmusym, muname, c.compname, c.comppct_r, month, flodfreqcl, pondfreqcl, soimoistdept_l, soimoistdept_r, soimoistdept_h, soimoistdepb_l, soimoistdepb_r, soimoistdepb_h, soimoiststat
       
-  FROM mapunit mu INNER JOIN
-       component c ON c.mukey = mu.mukey LEFT OUTER JOIN
+  FROM legend l INNER JOIN
+       mapunit mu ON mu.lkey = l.lkey INNER JOIN",
+       if (duplicates == FALSE) {
+          "(SELECT MIN(nationalmusym) nationalmusym2, MIN(mukey) AS mukey2 
+          FROM mapunit
+          GROUP BY nationalmusym) AS mu2 ON mu2.nationalmusym2 = mu.nationalmusym INNER JOIN
+          (SELECT compname, comppct_r, cokey, mukey AS mukey2 FROM component) AS c ON c.mukey2 = mu2.mukey2"
+          } else {"(SELECT compname, comppct_r, cokey, mukey AS mukey2 FROM component) AS c ON c.mukey2 = mu.mukey"}
+       , "LEFT OUTER JOIN
        comonth cm ON cm.cokey = c.cokey LEFT OUTER JOIN
        cosoilmoist csm ON csm.comonthkey = cm.comonthkey
   
-  WHERE mu.mukey IN ('", paste0(mukey, collapse = "', '"), "')
+  WHERE ", WHERE,
 
-  ORDER BY muname, comppct_r DESC, compname DESC, month, soimoistdept_r
+  "ORDER BY mu.muname, comppct_r DESC, compname DESC, month, soimoistdept_r ASC
   ;")
   
   
@@ -17,7 +26,7 @@ get_cosoilmoist_from_SDA_db <- function(mukey, impute = TRUE){
   
   
   # recode metadata domains
-  d.cosoilmoist <- .metadata_replace(d.cosoilmoist, NASIS = FALSE)
+  d.cosoilmoist <- uncode(d.cosoilmoist, NASIS = FALSE)
   
   
   # cache original column names
