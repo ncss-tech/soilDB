fe2b426dc39956abd7bd590a8449657ba30b6ca9
Stephen Roecker <stephen.roecker@gmail.com>
2017-05-09 16:04:27 -0400

    adding get_projectmapunit_from_NASIS.R

---
+++ R/get_projectmapunit_from_NASIS.R
@@ -0,0 +1,34 @@
+get_projectmapunit_from_NASIS <- function() {
+  # must have RODBC installed
+  if(!requireNamespace('RODBC'))
+    stop('please install the `RODBC` package', call.=FALSE)
+  
+  q <- paste("SELECT p.projectiid, p.uprojectid, p.projectname, a2.areasymbol, lmu.musym, lmu.lmapunitiid AS mukey, mu.nationalmusym, muname, muacres
+             
+             FROM project_View_1 p INNER JOIN
+             projectmapunit pmu ON pmu.projectiidref = p.projectiid INNER JOIN 
+             mapunit mu ON mu.muiid = pmu.muiidref INNER JOIN
+             lmapunit lmu ON lmu.muiidref = mu.muiid INNER JOIN
+             legend l ON l.liid = lmu.liidref
+             INNER JOIN 
+             area a ON a.areaiid = p.mlrassoareaiidref
+             INNER JOIN
+             area a2 ON a2.areaiid = l.areaiidref
+             
+             WHERE l.legendsuituse != 1 OR l.legendsuituse IS NULL
+             
+             ORDER BY p.projectname, a.areasymbol, lmu.musym;"
+  )
+  
+  # setup connection local NASIS
+  channel <- RODBC::odbcDriverConnect(connection="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y")
+  
+  # exec query
+  d <- RODBC::sqlQuery(channel, q, stringsAsFactors=FALSE)
+  
+  # close connection
+  RODBC::odbcClose(channel)
+  
+  # done
+  return(d)
+  }
