50393575c3ace9fcb154dca7b235bf6472dadaf4
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-03-09 13:02:47 -0800

    splicing-in pedon linkages for western US, thanks Jay and Kent Sutcliffe

--- misc/make-scan-snotel-metadata-db.R
+++ misc/make-scan-snotel-metadata-db.R
@@ -1,8 +1,9 @@
+library(plyr)
+
 # https://github.com/ncss-tech/soilDB/issues/61
 
 ## get these data from SCAN www map, zoom all the way out and then click on export to CSV
-
-x <- read.csv('scan-snotel-metadata.csv', stringsAsFactors = FALSE)
+x <- read.csv('scan-snotel-data/station-metadata.csv', stringsAsFactors = FALSE)
 
 # fix HUC formatting
 x$HUC <- as.character(as.numeric(x$HUC))
@@ -13,7 +14,35 @@ names(x)[2] <- 'Site'
 # check: OK
 str(x)
 
+
+## most of these files are maintained by regional staff
+## naming convention from NASIS site table
+p <- read.csv('scan-snotel-data/Utah_DCO_Soil_Lab_Data.csv', stringsAsFactors = FALSE)
+
+# whats in here:
+# many sites from several states! 
+str(p)
+table(p$state)
+
+# keep subset of columns
+p <- p[, c('climstaid', 'climstanm', 'upedonid', 'pedlabsampnum')]
+
+# re-name ID
+names(p)[1] <- 'Site'
+
+# check: ok
+str(p)
+
+
+## combine
+SCAN_SNOTEL_metadata <- join(x, p, by='Site', type='left')
+
+# check for possible errors via station name comparison
+idx <- which( ! SCAN_SNOTEL_metadata$Name == SCAN_SNOTEL_metadata$climstanm )
+SCAN_SNOTEL_metadata[idx, c('Site', 'Name', 'climstanm')]
+
+# hmm... mostly abbreviations and spelling
+
 # save as R data file
-SCAN_SNOTEL_metadata <- x
 save(SCAN_SNOTEL_metadata, file='../data/SCAN_SNOTEL_metadata.rda')
 
