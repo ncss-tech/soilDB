7a4721e7fee126dd585966a8d9a47e4f28b1cb72
Dylan Beaudette <dylan.beaudette@gmail.com>
2018-03-14 10:54:54 -0700

    safer mechanism for setting options #54

--- R/AAAA.R
+++ R/AAAA.R
@@ -2,15 +2,20 @@
 # setup a new environment to store error messages, etc.
 soilDB.env <- new.env(hash=TRUE, parent = parent.frame())
 
-# define some default options
-options(soilDB.verbose=FALSE)
-
-# set default local nasis authentication
-options(soilDB.NASIS.credentials="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y")
-
-# update according to win 7 or 10
-si <- Sys.info()
-if( grepl('windows', si['sysname'], ignore.case = TRUE) & grepl('8', si['release'], ignore.case = TRUE) ) {
-  options(soilDB.NASIS.credentials="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y365")
+# safely register some options at package load time
+.onLoad <- function(libname, pkgname) {
+  # function verbosity
+  options(soilDB.verbose=FALSE)
+  
+  # set default local nasis authentication
+  options(soilDB.NASIS.credentials="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y")
+  
+  # update according to win 7 or 10
+  si <- Sys.info()
+  if( grepl('windows', si['sysname'], ignore.case = TRUE) & grepl('8', si['release'], ignore.case = TRUE) ) {
+    options(soilDB.NASIS.credentials="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y365")
+  }
 }
+
+
 
