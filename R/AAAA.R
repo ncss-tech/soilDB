abcfc447609e6dfd608bfa76bf08e6e77f2997a4
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-08-13 14:05:12 -0700

    bug fix for mysterious re-opening of #54

--- R/AAAA.R
+++ R/AAAA.R
@@ -12,7 +12,7 @@ soilDB.env <- new.env(hash=TRUE, parent = parent.frame())
   
   # update according to win 7 or 10
   si <- Sys.info()
-  if( grepl('windows', si['sysname'], ignore.case = TRUE) & grepl('8', si['release'], ignore.case = TRUE) ) {
+  if( grepl('windows', si['sysname'], ignore.case = TRUE) & grepl('8|10', si['release'], ignore.case = TRUE) ) {
     options(soilDB.NASIS.credentials="DSN=nasis_local;UID=NasisSqlRO;PWD=nasisRe@d0n1y365")
   }
 }
