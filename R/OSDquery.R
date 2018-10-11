9cacf7004672451106f4c0c5947e6de4553673fc
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-10-11 14:18:02 -0700

    more documentation. not ready for CRAN but package should install

--- R/OSDquery.R
+++ R/OSDquery.R
@@ -23,6 +23,7 @@ OSDquery <- function(mlra='', taxonomic_class='', typical_pedon='', brief_narrat
                   geog_assoc_soils=geog_assoc_soils
                   )
   
+  ## TODO: move to load-balanced URL
   # API URL
   u <- 'http://soilmap2-1.lawr.ucdavis.edu/osd-search/index.php'
   
