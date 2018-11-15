3421a21fc7a839edaa035ade1b18145e922d0026
Dylan Beaudette <debeaudette@ucdavis.edu>
2018-11-14 23:06:21 -0800

    first version of this function, new interface to series co-occurence data

---
+++ R/siblings.R
@@ -0,0 +1,68 @@
+
+
+# 2018-11-14
+# return information on soil series that co-occur with `s`
+# component.data: should the component names, kind, percent, etc. be returned as well?
+# recursive: should sibling data be queried for the 1st set of siblings?
+siblings <- function(s, component.data=FALSE, recursive=FALSE) {
+  
+  # helper functions
+  .getSibling <- function(i) {
+    # these use the new API
+    u <- sprintf('http://soilmap2-1.lawr.ucdavis.edu/api/soil-series.php?q=siblings&s=%s', i)
+    
+    # attempt query to API for basic sibling set, result is JSON
+    sib <- try(jsonlite::fromJSON(u))[[1]]
+    
+    # TODO, handle errors
+    
+    # result is FALSE if no matching data
+    return(sib)
+  }
+  
+  .getSiblingData <- function(i) {
+    # these use the new API
+    u <- sprintf('http://soilmap2-1.lawr.ucdavis.edu/api/soil-series.php?q=sibling_data&s=%s', i)
+    
+    # attempt query to API for component data, result is JSON
+    sib <- try(jsonlite::fromJSON(u))[[1]]
+    
+    # TODO, handle errors
+    
+    # result is FALSE if no matching data
+    return(sib)
+  }
+  
+  # init output as list
+  res <- list()
+  
+  # sanity check
+  if( !requireNamespace('jsonlite', quietly=TRUE))
+    stop('please install the `jsonlite` package', call.=FALSE)
+  
+  # get basic data
+  res$sib <- .getSibling(s)
+  
+  # optionally get data
+  if(component.data) {
+    res$sib.data <- .getSiblingData(s)
+  }
+  
+  # optionally get second set of siblings
+  # flatten into single DF
+  if(recursive) {
+    second.sib <- lapply(res$sib$sibling, .getSibling)
+    res$second.sib <- do.call('rbind', second.sib)
+    
+    # data too?
+    if(component.data) {
+      second.sib.data <- lapply(res$sib$sibling, .getSiblingData)
+      res$second.sib.data <- do.call('rbind', second.sib.data)
+    }
+  }
+  
+  # TODO: error checking and further sanity checks
+  return(res)
+}
+
+
