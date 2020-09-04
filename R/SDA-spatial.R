167200a003038d08da65db19d1b6251e520bb6ca
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-09-04 09:48:23 -0700

    deprecating older SDA spatial query functions, this will affect the SDA tutorial

--- R/SDA-spatial.R
+++ R/SDA-spatial.R
@@ -15,6 +15,23 @@
 ## TODO: test with geom appearing in different positions within query results
 ## TODO: geometry collections are not allowed in sp objects..
 ## TODO: consider moving to sf
+
+#' @title Post-process WKT returned from SDA.
+#' 
+#' @description This is a helper function, commonly used with \code{SDA_query} to extract WKT (well-known text) representation of geometry to an sp-class object.
+#' 
+#' @param d \code{data.frame} returned by \code{SDA_query}, containing WKT representation of geometry
+#' @param g name of column in \code{d} containing WKT geometry
+#' @param p4s PROJ4 CRS defintion, typically GCS WGS84
+#' 
+#' @details The SDA website can be found at \url{http://sdmdataaccess.nrcs.usda.gov}. See the \href{http://ncss-tech.github.io/AQP/soilDB/SDA-tutorial.html}{SDA Tutorial} for detailed examples.
+#' 
+#' @note This function requires the `httr`, `jsonlite`, `XML`, and `rgeos` packages.
+#' 
+#' @author D.E. Beaudette
+#' 
+#' @return A \code{Spatial*} object.
+#' 
 processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
   # iterate over features (rows) and convert into list of SPDF
   p <- list()
@@ -24,7 +41,6 @@ processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
   # looking at the first feature for efficiency, all others should be the same
   g.type <- class(rgeos::readWKT(d[1, g]))
   
-  # pb <- txtProgressBar(style = 3)
   for(i in seq(1, n)) {
     # extract the current row in the DF
     d.i <- d[i, ] 
@@ -46,9 +62,7 @@ processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
     names(s.i) <- c('gid', names(d.i))
     # save to list
     p[[i]] <- s.i
-    # setTxtProgressBar(pb, i/n)
   }
-  # close(pb)
   
   # reduce list to single SPDF
   spdf <- do.call('rbind', p)
@@ -70,7 +84,7 @@ processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
 
 #' @title SDA Spatial Query
 #' 
-#' @description Query SDA (SSURGO / STATSGO) records via spatial intersection with supplied geometries. Input can be SpatialPoints, SpatialLines, or SpatialPolygons objects with a valid CRS. Map unit keys, overlapping polygons, or the spatial intersectionion of `geom` + SSURGO polygons can be returned. See details.
+#' @description Query SDA (SSURGO / STATSGO) records via spatial intersection with supplied geometries. Input can be SpatialPoints, SpatialLines, or SpatialPolygons objects with a valid CRS. Map unit keys, overlapping polygons, or the spatial intersectionion of `geom` + SSURGO / STATSGO polygons can be returned. See details.
 #' 
 #' @param geom a Spatial* object, with valid CRS. May contain multiple features.
 #' @param what a character vector specifting what to return. `mukey`: `data.frame` with intersecting map unit keys and names, `geom` overlapping or intersecting map unit polygons
@@ -81,31 +95,38 @@ processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
 #'
 #' @return A `data.frame` if `what` is 'mukey', otherwise `SpatialPolygonsDataFrame` object.
 #' 
-#' @author D.E. Beaudette
+#' @author D.E. Beaudette, A.G. Brown, D.R. Schlaepfer
 #' @seealso \code{\link{SDA_query}}
 #' @keywords manip
 #' 
+#' @aliases SDA_make_spatial_query,SDA_query_features
+#' 
 #' @note Row-order is not preserved across features in `geom` and returned object. Use `sp::over()` or similar functionality to extract from results.
 #' 
-#' @details Queries for map unit keys are always more efficient vs. queries for overlapping or intersecting (i.e. least efficient) features. `geom` is converted to GCS / WGS84 as needed.
+#' @details Queries for map unit keys are always more efficient vs. queries for overlapping or intersecting (i.e. least efficient) features. `geom` is converted to GCS / WGS84 as needed. Map unit keys are always returned when using \code{what = "geom"}.
 #' 
 #' There is a 100,000 record limit and 32Mb JSON serializer limit, per query.
 #' 
-#' SSURGO (detailed soil survey) and STATSGO (generalized soil survey) data are stored together within SDA. This means that queries that don't specify an area symbol may result in a mixture of SSURGO and STATSGO records. See the examples below and the \href{http://ncss-tech.github.io/AQP/soilDB/SDA-tutorial.html}{SDA Tutorial} for details.
+#' SSURGO (detailed soil survey, typicall 1:24,000 scale) and STATSGO (generalized soil survey, 1:250,000 scale) data are stored together within SDA. This means that queries that don't specify an area symbol may result in a mixture of SSURGO and STATSGO records. See the examples below and the \href{http://ncss-tech.github.io/AQP/soilDB/SDA-tutorial.html}{SDA Tutorial} for details.
 #' 
 #' 
 #' @examples
 #' \donttest{
 #' if(requireNamespace("curl") &
 #'    curl::has_internet() & 
-#'    requireNamespace("sp")) {
+#'    requireNamespace("sp") &
+#'    requireNamespace("raster") 
+#'    ) {
 #' 
-#'    library(aqp)
-#'    library(sp)
+#' library(aqp)
+#' library(sp)
+#' library(raster)
+#' 
+#' ## query at a point
 #' 
 #' # example point
 #' p <- SpatialPoints(cbind(x = -119.72330, y = 36.92204), 
-#'                          proj4string = CRS('+proj=longlat +datum=WGS84'))
+#'                    proj4string = CRS('+proj=longlat +datum=WGS84'))
 #' 
 #' # query map unit records at this point
 #' res <- SDA_spatialQuery(p, what = 'mukey')
@@ -135,9 +156,98 @@ processSDA_WKT <- function(d, g='geom', p4s='+proj=longlat +datum=WGS84') {
 #'         plot.depth.axis=FALSE, hz.depths=TRUE, 
 #'         name.style='center-center'
 #' )
-#'    
+#' 
+#' 
+#' 
+#' ## query mukey + geometry that intersect with a bounding box
+#' 
+#' # define a bounding box: xmin, xmax, ymin, ymax
+#' #
+#' #         +-------------------(ymax, xmax)
+#' #         |                        |
+#' #         |                        |
+#' #     (ymin, xmin) ----------------+
+#' b <- c(-119.747629, -119.67935, 36.912019, 36.944987)
+#' 
+#' # convert bounding box to WKT
+#' bbox.sp <-as(extent(b), 'SpatialPolygons')
+#' proj4string(bbox.sp) <- '+proj=longlat +datum=WGS84'
+#' 
+#' # results contain associated map unit keys (mukey)
+#' # return SSURGO polygons, after intersection with provided BBOX
+#' ssurgo.geom <- SDA_spatialQuery(
+#'   bbox.sp, 
+#'   what = 'geom', 
+#'   db = 'SSURGO', 
+#'   geomIntersection = TRUE
+#' )
+#' 
+#' # return STATSGO polygons, after intersection with provided BBOX
+#' statsgo.geom <- SDA_spatialQuery(
+#'   bbox.sp, 
+#'   what = 'geom', 
+#'   db = 'STATSGO', 
+#'   geomIntersection = TRUE
+#' )
+#' 
+#' # inspect results
+#' par(mar = c(0,0,3,1))
+#' plot(ssurgo.geom, border = 'royalblue')
+#' lines(statsgo.geom, lwd = 2, col = 'firebrick')
+#' lines(bbox.sp, lwd = 3)
+#' legend(
+#'   x = 'top', 
+#'   legend = c('BBOX', 'STATSGO', 'SSURGO'), 
+#'   lwd = c(3, 2, 1),
+#'   col = c('black', 'firebrick', 'royalblue'),
+#'   horiz = TRUE, 
+#'   bty = 'n'
+#' )
+#' 
+#' # quick reminder that STATSGO map units often contain many components
+#' # format an SQL IN statement using the first STATSGO mukey
+#' mu.is <- format_SQL_in_statement(statsgo.geom$mukey[1])
+#' 
+#' # composite SQL WHERE clause
+#' sql <- sprintf("mukey IN %s", mu.is)
+#' 
+#' # get commonly used map unit / component / chorizon records
+#' # as a SoilProfileCollection object
+#' x <- fetchSDA(sql)
+#' 
+#' # tighter figure margins
+#' par(mar = c(0,0,3,1))
+#' 
+#' 
+#' # organize component sketches by national map unit symbol
+#' # color horizons via awc
+#' # adjust legend title
+#' # add alternate label (vertical text) containing component percent
+#' # move horizon names into the profile sketches
+#' # make profiles wider
+#' groupedProfilePlot(
+#'   x, 
+#'   groups = 'nationalmusym', 
+#'   label = 'compname', 
+#'   color = 'awc_r', 
+#'   col.label = 'Available Water Holding Capacity (cm / cm)',
+#'   alt.label = 'comppct_r',
+#'   name.style = 'center-center',
+#'   width = 0.3
+#' )
+#' 
+#' 
+#' mtext(
+#'   'STATSGO (1:250,000) map units contain a lot of components!', 
+#'   side = 1, 
+#'   adj = 0, 
+#'   line = -1.5, 
+#'   at = 0.25, 
+#'   font = 4
+#' )
 #'  }
 #' }
+#' 
 SDA_spatialQuery <- function(geom, what='mukey', geomIntersection=FALSE,
   db = c("SSURGO", "STATSGO")) {
 
@@ -233,10 +343,10 @@ SDA_spatialQuery <- function(geom, what='mukey', geomIntersection=FALSE,
 }
 
 
-## TODO: there is a lot of overhead connecting to SDA, large queries are best done in bulk!
-# this is intended for those cases where a result is required for each feature processed
-# i is a Spatial* object with valid CRS, a single feature or multiple (converted to a geometry collection)
+## now deprecated
 SDA_make_spatial_query <- function(i) {
+  
+  .Deprecated(new = 'SDA_spatialQuery')
   
   # check for required packages
   if(!requireNamespace('rgeos', quietly = TRUE))
@@ -267,7 +377,10 @@ SDA_make_spatial_query <- function(i) {
 # note that it is a very slow for large collections
 # x is a Spatial* object with more than 1 feature
 # id is the name of an attribute that contains a unique ID for each feature
+## now deprecated
 SDA_query_features <- function(x, id='pedon_id') {
+  
+  .Deprecated(new = 'SDA_spatialQuery')
   
   # sanity check: ensure that the ID is unique
   if(length(x[[id]]) != length(unique(x[[id]])))
