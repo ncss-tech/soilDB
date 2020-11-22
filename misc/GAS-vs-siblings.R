5fa6f8d2ab02a7d6c00ab715f07cb535d183dc95
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-21 23:35:18 -0800

    Update GAS-vs-siblings.R

--- misc/GAS-vs-siblings.R
+++ misc/GAS-vs-siblings.R
@@ -1,9 +1,41 @@
 library(aqp)
 library(soilDB)
+library(venn)
+
+s <- 'holland'
+o <- fetchOSD(s, extended = TRUE)
+
+# if missing, `geog_assoc_soils` is FALSE --> should probably be an emtpy DF
+gas <- o$geog_assoc_soils$gas
+sib <- siblings(s)
+
+sib.all <- toupper(unique(sib$sib$sibling))
+sib.mag <- toupper(unique(sib$sib$sibling[sib$sib$majcompflag]))
+
+l <- list(
+  `Geog.\nAssoc. Soils\n` = gas,
+  `Siblings (All)` = sib.all,
+  `Siblings (MC)` = sib.mag
+)
+
+par(mar = c(0, 0, 2, 0))
+venn(l, ellipse = FALSE, ilcs = 1, par = FALSE)
+title(toupper(s))
 
 
+l <- list(
+  `Geog.\nAssoc. Soils\n` = gas,
+  `Siblings (All)` = sib.all,
+  `Siblings (MC)` = sib.mag,
+  `Competing` = o$competing$competing
+)
 
-## GAS vs. siblings
+par(mar = c(0, 0, 2, 0))
+venn(l, ellipse = TRUE, ilcs = 1, par = FALSE)
+title(toupper(s))
+
+
+# search for series with no specified GAS
 
 o <- OSDquery(geog_assoc_soils = 'none')
 
