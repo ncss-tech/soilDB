e815e54f906881e05c628ce5d9edf7738df1f25e
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-22 21:02:15 -0800

    Update GAS-vs-siblings.R

--- misc/GAS-vs-siblings.R
+++ misc/GAS-vs-siblings.R
@@ -2,15 +2,18 @@ library(aqp)
 library(soilDB)
 library(venn)
 
-s <- 'holland'
+s <- 'drummer'
 o <- fetchOSD(s, extended = TRUE)
 
 # if missing, `geog_assoc_soils` is FALSE --> should probably be an emtpy DF
 gas <- o$geog_assoc_soils$gas
 sib <- siblings(s)
+cous <- siblings(s, cousins = TRUE)
 
 sib.all <- toupper(unique(sib$sib$sibling))
 sib.mag <- toupper(unique(sib$sib$sibling[sib$sib$majcompflag]))
+
+cousins <- toupper(unique(cous$cousins$sibling))
 
 l <- list(
   `Geog.\nAssoc. Soils\n` = gas,
@@ -25,6 +28,19 @@ title(toupper(s))
 
 l <- list(
   `Geog.\nAssoc. Soils\n` = gas,
+  `Siblings (All)` = sib.all,
+  `Siblings (MC)` = sib.mag,
+  `Competing` = o$competing$competing
+)
+
+par(mar = c(0, 0, 2, 0))
+venn(l, ellipse = TRUE, ilcs = 1, par = FALSE)
+title(toupper(s))
+
+
+l <- list(
+  `Geog.\nAssoc. Soils\n` = gas,
+  `Cousins` = cousins,
   `Siblings (All)` = sib.all,
   `Siblings (MC)` = sib.mag,
   `Competing` = o$competing$competing
