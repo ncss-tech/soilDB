beeeafd6c7ae8f9490af7d8fe1a91311dfbb03d3
Dylan Beaudette <debeaudette@ucdavis.edu>
2020-11-23 14:48:00 -0800

    minor updates

--- misc/GAS-vs-siblings.R
+++ misc/GAS-vs-siblings.R
@@ -10,44 +10,50 @@ gas <- o$geog_assoc_soils$gas
 sib <- siblings(s)
 cous <- siblings(s, cousins = TRUE)
 
+# unique vectors / normalized to upper case
 sib.all <- toupper(unique(sib$sib$sibling))
+# just siblings that are major components
 sib.mag <- toupper(unique(sib$sib$sibling[sib$sib$majcompflag]))
+# cousins that are major components
+cousins <- toupper(unique(cous$cousins$sibling[cous$cousins$majcompflag]))
 
-cousins <- toupper(unique(cous$cousins$sibling))
+# d.all <- data.frame(series = sib.all)
+# d.maj <- data.frame(series = sib.mag)
+# d.gas <- data.frame(series = gas)
 
 l <- list(
   `Geog.\nAssoc. Soils\n` = gas,
-  `Siblings (All)` = sib.all,
-  `Siblings (MC)` = sib.mag
+  `Siblings\n(All)` = sib.all,
+  `Siblings\n(Maj. Components)` = sib.mag
 )
 
 par(mar = c(0, 0, 2, 0))
-venn(l, ellipse = FALSE, ilcs = 1, par = FALSE)
+venn(l, ellipse = FALSE, ilcs = 1, sncs = 0.66, par = FALSE, zcolor = 'style')
 title(toupper(s))
 
 
 l <- list(
   `Geog.\nAssoc. Soils\n` = gas,
-  `Siblings (All)` = sib.all,
-  `Siblings (MC)` = sib.mag,
-  `Competing` = o$competing$competing
+  `Siblings\n(All)` = sib.all,
+  `Siblings\n(Maj. Components)` = sib.mag,
+  `Competing\nSeries` = o$competing$competing
 )
 
 par(mar = c(0, 0, 2, 0))
-venn(l, ellipse = TRUE, ilcs = 1, par = FALSE)
+venn(l, ellipse = TRUE, ilcs = 1, sncs = 0.66, par = FALSE, zcolor = 'style')
 title(toupper(s))
 
 
 l <- list(
   `Geog.\nAssoc. Soils\n` = gas,
-  `Cousins` = cousins,
-  `Siblings (All)` = sib.all,
-  `Siblings (MC)` = sib.mag,
-  `Competing` = o$competing$competing
+  `Cousins\n(Maj. Components)` = cousins,
+  `Siblings\n(All)` = sib.all,
+  `Siblings\n(Maj. Components)` = sib.mag,
+  `Competing\nSeries` = o$competing$competing
 )
 
 par(mar = c(0, 0, 2, 0))
-venn(l, ellipse = TRUE, ilcs = 1, par = FALSE)
+venn(l, ellipse = TRUE, ilcs = 1, sncs = 0.66, par = FALSE, zcolor = 'style')
 title(toupper(s))
 
 
