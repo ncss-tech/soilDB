f7d2882f6798fe584e00a2bb0ca19419ce6dea25
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-01-04 16:53:52 -0800

    Use local_NASIS_defined everywhere w/ odbc::odbcListDataSources

--- R/fetchVegdata.R
+++ R/fetchVegdata.R
@@ -4,7 +4,7 @@
 fetchVegdata <- function(SS=TRUE, stringsAsFactors = default.stringsAsFactors()) {
 	
   # test connection
-  if(! 'nasis_local' %in% names(RODBC::odbcDataSources()))
+  if (!local_NASIS_defined())
     stop('Local NASIS ODBC connection has not been setup. Please see `http://ncss-tech.github.io/AQP/soilDB/setup_local_nasis.html`.')
 	
 	# 1. load data in pieces
