8f165ac3cc4c8fedca86ec2d4621008cbed5f4ed
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-01-11 20:31:41 -0800

    mukey.wcs: add basic (very small extent) test

---
+++ tests/testthat/test-mukey-WCS.R
@@ -0,0 +1,14 @@
+test_that("mukey.wcs works", {
+
+  x <- NULL
+
+  expect_silent(suppressWarnings({
+
+    x <- mukey.wcs(aoi = list(aoi = c(-114.16, 47.655, -114.155, 47.66),
+                              crs = '+init=epsg:4326'),
+                   db = 'gnatsgo', quiet = TRUE)
+
+  }))
+
+  expect_true(inherits(x, 'RasterLayer'))
+})
