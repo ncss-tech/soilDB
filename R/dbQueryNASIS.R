1f78db8e5f567f2addaca056522330fe04909f5e
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-02-17 16:26:54 -0800

    dbQueryNASIS: vectorize/test; dbConnectNASIS: add NASIS() alias

--- R/dbQueryNASIS.R
+++ R/dbQueryNASIS.R
@@ -3,30 +3,38 @@
 #' Send queries to a NASIS DBIConnection
 #' 
 #' 
-#' @param conn A \code{DBIConnection} object, as returned by
-#' \code{DBI::dbConnect()}.
-#' @param q A statement to execute using \code{DBI::dbGetQuery}
+#' @param conn A \code{DBIConnection} object, as returned by \code{DBI::dbConnect()}.
+#' @param q A statement to execute using \code{DBI::dbGetQuery}; or a (named) vector containing multiple statements to evaluate separately
 #' @param close Close connection after query? Default: \code{TRUE}
 #' @param ... Additional arguments to \code{DBI::dbGetQuery}
 #' @return Result of \code{DBI::dbGetQuery}
 #' @export dbQueryNASIS
 dbQueryNASIS <- function(conn, q, close = TRUE, ...) {
-
+  
   if (inherits(conn, 'try-error'))
     stop("Failed to connect to NASIS database!")
   
-  ## exec query
-  d <- DBI::dbGetQuery(conn, q, ...)
-  # res <- DBI::dbSendQuery(conn, q)
-  # d <- DBI::dbFetch(res)
-  # d <- RODBC::sqlQuery(channel, q, stringsAsFactors=FALSE)
-  
-  dd <- data.frame(d)
-  ## close connection
-  if (close == TRUE)
+  # vectorize queries (return a [possibly named] list)
+  if(length(q) > 1) {
+    # recursively call dbQueryNASIS(close=FALSE)
+    res <- lapply(q, function(x) dbQueryNASIS(conn, x, close = FALSE))
+    names(res) <- names(q)
+    dd <- res
+  } else {
+    ## exec query
+    d <- DBI::dbGetQuery(conn, q, ...)
+    # res <- DBI::dbSendQuery(conn, q)
+    # d <- DBI::dbFetch(res)
+    # d <- RODBC::sqlQuery(channel, q, stringsAsFactors=FALSE)
+    
+    dd <- data.frame(d)
+    
+  }
+
+  ## close connection if needed
+  if (close) {
     DBI::dbDisconnect(conn)
-  # RODBC::odbcClose(channel)
-  
+  }
   return(dd)
 }
 
@@ -34,9 +42,9 @@ dbQueryNASIS <- function(conn, q, close = TRUE, ...) {
 
 #' Create a connection to a local NASIS database
 #' 
-#' Create a connection to a local NASIS database
+#' Create a connection to a local NASIS database with `DBI`
 #' 
-#' 
+#' @aliases NASIS
 #' @param static_path Optional: path to SQLite database containing NASIS table
 #' structure; Default: \code{NULL}
 #' @return A \code{DBIConnection} object, as returned by
@@ -50,3 +58,5 @@ dbConnectNASIS <- function(static_path = NULL) {
   
   return(res)
 }
+
+NASIS <- function(static_path = NULL) dbConnectNASIS(static_path = static_path)
