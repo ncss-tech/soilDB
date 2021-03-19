46527ef3574f83c88a11c23fc200c9e901f62db6
Andrew G. Brown <andrew.g.brown@usda.gov>
2021-03-19 08:52:31 -0700

    Add demo to /misc

---
+++ misc/fetchOSD_JSON-demo.R
@@ -0,0 +1,35 @@
+library(soilDB)
+library(tibble)
+
+# list all
+series <- gsub("(.*).json", "\\1", basename(list.files("~/workspace/SoilKnowledgeBase/inst/extdata/OSD",
+                                                       recursive = TRUE)))
+
+# smaller test example
+#series <- c("Ackwater","Ariel","Neissenberg","Meikle","foo",NA)
+
+# this uses a _local_ cloned instance of SKB rep--fast (base_url = NULL or missing will use GitHub)
+res <- fetchOSD_JSON(series, base_url = "~/workspace/SoilKnowledgeBase/inst/extdata/OSD")
+
+# 2023 series are "incomplete" in one or more sections
+tibble(series = series[match(res$SERIES, toupper(series))],
+       complete = complete.cases(res))  %>%
+  subset(!complete)
+
+# tabulate "missing" sections
+apply(res, 2, function(x) sum(is.na(x)))
+
+(tibble(res) %>%
+  subset(is.na(TAXONOMIC.CLASS)))$SERIES
+
+(tibble(res) %>%
+    subset(is.na(TYPICAL.PEDON)))$SERIES
+
+# most common states
+allstates <- table(unlist(strsplit(res$STATES, ",")))
+
+# count by state
+allstates[order(allstates, decreasing = TRUE)]
+
+# proportion by state
+round(prop.table(allstates[order(allstates, decreasing = TRUE)]), 2)
