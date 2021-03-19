9de324f374f7ea3975670d7f3784ba052b561e55
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-03-19 16:21:22 -0700

    Add WCS/SDA viz to demo

--- misc/ca750-newpedons.R
+++ misc/ca750-newpedons.R
@@ -2,7 +2,9 @@
 library(soilDB)
 library(sharpshootR)
 library(sf)
+library(raster)
 
+# use DBI connection to create a NASIS table list object and select some columns
 s.sub <- soilDB::createStaticNASIS("site_View_1")[["site_View_1"]][, c(
   "usiteid",
   "plssmeridian",
@@ -15,18 +17,20 @@ s.sub <- soilDB::createStaticNASIS("site_View_1")[["site_View_1"]][, c(
   "aspect"
 )]
 
-colnames(s.sub) <- c("id",
-                     "m","t","r","s","plsssdetails",
+# rename some columns to match PLSS2LL() data.frame format
+colnames(s.sub) <- c("id","m","t","r","s","plsssdetails",
                      "elev", "slope", "aspect")
-s.sub <- subset(s.sub, !grepl("meters", plsssdetails))
+
+# add meridian section style etc
+s.sub$plsssdetails[grepl("meters", s.sub$plsssdetails)] <- ""
+s.sub$qq <- ""#sapply(strsplit(s.sub$plsssdetails, ","), function(x) trimws(x[1]))
+s.sub$q <- ""#sapply(strsplit(s.sub$plsssdetails, ","), function(x) trimws(x[2]))
+
 s.sub$m <- "CA21"
 s.sub$type <- "SN"
 
 is_corner <- grepl("corner$|section$|corner of$", s.sub$plsssdetails)
 corners <- gsub("(.*) corner$|(.*) section$|(.*) corner of$", "\\1\\2\\3", s.sub$plsssdetails)
-
-s.sub$qq <- sapply(strsplit(s.sub$plsssdetails, ","), function(x) trimws(x[1]))
-s.sub$q <- sapply(strsplit(s.sub$plsssdetails, ","), function(x) trimws(x[2]))
 
 s.complete <- s.sub[complete.cases(s.sub) & 
                       !s.sub$t == "NA" &
@@ -47,8 +51,36 @@ s.complete <- s.complete[!grepl("NA", s.complete$plssid),]
 # fix(s.complete)
 res <- PLSS2LL(s.complete)
 
-pts <- st_as_sf(res[complete.cases(res),], coords = c("lon", "lat"), 
+# convert the longlat points from PLSS to sf
+pts <- st_as_sf(res[complete.cases(res),], 
+                coords = c("lon", "lat"), 
                 crs = st_crs(4326))
+
+# get SSA polygon
 bdy <- fetchSDA_spatial("CA750", geom.src = "sapolygon")
-plot(st_as_sf(bdy)$geometry)
-plot(pts, add=T)
+
+# get raster of mukeys
+mukeyras <- mukey.wcs(ssa)
+
+# get data from SDA
+ssurgo <- SDA_query(sprintf(
+                    "SELECT * FROM legend 
+                    INNER JOIN mapunit ON legend.lkey = mapunit.lkey
+                    WHERE mukey IN %s", format_SQL_in_statement(unique(values(ras)))))
+
+# get just target survey area
+ssurgo.sub <- subset(ssurgo, areasymbol == "CA750")
+
+# get the numbers only out
+ssurgo.sub$musym <- as.numeric(gsub("(\\d+)|.*","\\1", ssurgo.sub$musym))
+
+# new raster attribute table based on numeric musyms used in (most of) CA750
+rat <- merge(levels(mukeyras)[[1]], ssurgo.sub[,c('mukey',"musym")], 
+             by.x = 'ID', by.y = 'mukey', 
+             sort = FALSE, all.x = TRUE, incomparables = NA)
+levels(mukeyras) <- rat
+
+# make a map
+plot(mukeyras, "musym")
+plot(st_transform(st_as_sf(bdy), crs(mukeyras))$geometry, add = TRUE)
+plot(st_transform(pts, crs(mukeyras))$geometry, add = TRUE)
