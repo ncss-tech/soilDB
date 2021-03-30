8befd8fc9617aeea0ae7d109a7415e9cc0590c28
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-03-30 10:11:45 -0700

    Renaming/update docs+tests

--- misc/get_OSD_JSON-demo.R
+++ misc/get_OSD_JSON-demo.R
@@ -10,7 +10,7 @@ series <- series[nchar(series) > 0]
 
 # this uses a _local_ cloned instance of SKB repo (pretty fast)
 #  - base_url = NULL or missing will use GitHub
-res <- fetchOSD_JSON(series, base_url = path)
+res <- get_OSD_JSON(series, base_url = path)
 
 # 2023 series are "incomplete" in one or more sections
 tibble(series = series[match(res$SERIES, toupper(series))], 
--- tests/testthat/test-get_OSD_JSON.R
+++ tests/testthat/test-get_OSD_JSON.R
@@ -1,4 +1,4 @@
-test_that("fetchOSD_JSON works", {
+test_that("get_OSD_JSON works", {
   
   skip_if_offline()
   
@@ -7,7 +7,7 @@ test_that("fetchOSD_JSON works", {
   series <- c("Musick", "Hector", NA, "Foobar", "Chewacla")
   
   # warning: F/FOOBAR.json does not exist
-  expect_warning({res <- fetchOSD_JSON(series)})
+  expect_warning({res <- get_OSD_JSON(series)})
   
   # data.frame result w/ 3 existing official series
   expect_equal(nrow(res), 3)
