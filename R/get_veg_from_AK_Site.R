15e3fb2496c403fce0e84aed394b57205320569d
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2021-03-30 11:02:53 -0700

    Fix get_veg_from_AK_site

--- R/get_veg_from_AK_Site.R
+++ R/get_veg_from_AK_Site.R
@@ -20,14 +20,18 @@ get_veg_from_AK_Site <- function(dsn) {
     stop('please install the `odbc` package', call.=FALSE)
   
 	# basic query
-	q <- "SELECT VegStop.usiteid as site_id, VegStop.fECOSITE as ecosite, localplant.lplantsym as plantsym, localplant.lplantname as plantname, vegetation.COVER as pct_cover
-  FROM 
-	(VegStop INNER JOIN vegetation ON VegStop.vegstopdbid = vegetation.vegstodbiidref)
-	INNER JOIN localplant ON vegetation.lplantsym = localplant.lplantuid
-	ORDER BY VegStop.usiteid ASC;"
+	q <- "SELECT VegStop.usiteid as site_id, 
+	             VegStop.fECOSITE as ecosite, 
+	             localplant.plantsym as plantsym, 
+	             localplant.lplantname as plantname, 
+	             vegetation.COVER as pct_cover
+          FROM 
+        	(VegStop INNER JOIN vegetation ON VegStop.vegstopdbid = vegetation.vegstodbiidref)
+        	INNER JOIN localplant ON vegetation.plantsym = localplant.plantiidref
+        	ORDER BY VegStop.usiteid ASC;"
   
 	# setup connection to our pedon database
-	channel <- dbConnect(odbc::odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", dsn))
+	channel <- DBI::dbConnect(odbc::odbc(), .connection_string = paste0("Driver={Microsoft Access Driver (*.mdb, *.accdb)};DBQ=", dsn))
 	
 	# exec query
 	d <- DBI::dbGetQuery(channel, q)
