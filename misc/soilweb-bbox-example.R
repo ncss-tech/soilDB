0038d0c79ac8d37e1e2dcc00450d7804a05d6e0d
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-06-30 12:12:17 -0700

    better example

--- misc/soilweb-bbox-example.R
+++ misc/soilweb-bbox-example.R
@@ -5,16 +5,57 @@ library(viridisLite)
 library(sf)
 
 
-bb <- '-121.8655 38.8834,-121.8655 39.0117,-121.5878 39.0117,-121.5878 38.8834,-121.8655 38.8834'
+bb <- '-91.6853 36.4617,-91.6853 36.5281,-91.5475 36.5281,-91.5475 36.4617,-91.6853 36.4617'
 wkt <- sprintf('POLYGON((%s))', bb)
 
 x <- st_as_sfc(wkt)
 st_crs(x) <- 4326
-plot(x)
+
+# double-check BBOX: ok
+mapview::mapview(x)
 
 
-# mapview::mapview(x)
+# get gSSURGO grid here
+mu <- mukey.wcs(aoi = x, db = 'gssurgo')
 
-(mu <- mukey.wcs(aoi = x, db = 'gnatsgo'))
+# unique map unit keys
+ll <- levels(mu)[[1]]
 
+# note SSA boundary
 levelplot(mu, att = 'ID', margin = FALSE, colorkey = FALSE, col.regions = viridis)
+
+# get thematic data from SDA
+# dominant component
+# depth-weighted average
+# sand, silt, clay (RV)
+p <-  get_SDA_property(property = c("sandtotal_r","silttotal_r","claytotal_r"),
+                       method = "Dominant Component (Numeric)", 
+                       mukeys = ll$ID,
+                       top_depth = 25,
+                       bottom_depth = 50)
+
+head(p)
+
+# re-create raster attribute table with aggregate soil properties
+rat <- merge(ll, p, by.x = 'ID', by.y = 'mukey', sort = FALSE, all.x = TRUE)
+
+# re-pack RAT
+levels(mu) <- rat
+
+# convert raster + RAT --> stack of values
+ssc <- deratify(mu, c("sandtotal_r","silttotal_r","claytotal_r"))
+
+# graphical check
+# note implicit simplification via maxpixels
+levelplot(
+  ssc, 
+  main = 'Sand, Silt, Clay (RV) 25-50cm\nDominant Component',
+  margin = FALSE, 
+  scales = list(draw = FALSE), 
+  col.regions = viridis,
+  maxpixels = 1e4
+)
+
+
+
+
