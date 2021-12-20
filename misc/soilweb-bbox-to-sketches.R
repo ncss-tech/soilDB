89be754ebe137d1d5feb25f1e3f71d7393b470e1
Dylan Beaudette <debeaudette@ucdavis.edu>
2021-12-20 12:04:16 -0800

    more examples and link to related idea

--- misc/soilweb-bbox-to-sketches.R
+++ misc/soilweb-bbox-to-sketches.R
@@ -114,3 +114,37 @@ plotProfileDendrogram(
 )
 
 
+## borrowing ideas from this tutorial:
+## https://ncss-tech.github.io/AQP/soilDB/exploring-geomorph-summary.html
+##
+hp.cols <- RColorBrewer::brewer.pal(n = 5, name = 'Set1')[c(2, 3, 4, 5, 1)]
+
+# re-order hillslope proportions according to clustering
+hp <- osd$hillpos[res$order, ]
+nm <- names(hp[, 2:6])
+
+par(mar = c(0.5, 0, 0, 2))
+layout(matrix(c(1,2)), widths = c(1,1), heights = c(2,1))
+plotProfileDendrogram(osd$SPC, res$clust, dend.y.scale = 3, scaling.factor = 0.012, y.offset = 0.2, width = 0.32, name.style = 'center-center', cex.names = 0.7, shrink = TRUE, cex.id = 0.55)
+
+matplot(y = hp[, 2:6], type = 'b', lty = 1, pch = 16, axes = FALSE, col = hp.cols, xlab = '', ylab = '', xlim = c(0.5, length(osd$SPC) + 1))
+# grid(nx = 0, ny = NULL)
+axis(side = 4, line = -1, las = 1, cex.axis = 0.7)
+# axis(side = 2, line = -3, las = 1, cex.axis = 0.7)
+legend('topleft', legend = rev(nm), col = rev(hp.cols), pch = 16, bty = 'n', cex = 0.8, pt.cex = 2, horiz = TRUE, inset = c(0.01, 0.01))
+mtext('Probability', side = 2, line = -2, font = 2)
+
+
+
+par(mar = c(0.5, 0, 0, 2))
+layout(matrix(c(1,2)), widths = c(1,1), heights = c(2,1))
+plotProfileDendrogram(osd$SPC, res$clust, dend.y.scale = 3, scaling.factor = 0.012, y.offset = 0.2, width = 0.32, name.style = 'center-center', cex.names = 0.7, shrink = TRUE, cex.id = 0.55)
+
+sp <- c(1.5, rep(1, times = length(osd$SPC) - 1))
+barplot(height = t(as.matrix(hp[, 2:6])), beside = FALSE, width = 0.5, space = sp, col = hp.cols,  axes = FALSE, xlab = '', ylab = '', xlim = c(0.5, length(osd$SPC) + 1), ylim = c(0, 1.2))
+
+legend(x = 0.75, y = 1.2, legend = rev(nm), col = rev(hp.cols), pch = 15, bty = 'n', cex = 0.8, pt.cex = 1.25, horiz = TRUE)
+mtext('Probability', side = 2, line = -2, font = 2)
+
+
+
