d1f191bcdc84ffd84eae77146ac606673b13db90
Andrew G. Brown <andrew.g.brown@ca.usda.gov>
2022-01-06 12:08:55 -0800

    get_SDV_legend_elements: add test and generalize for multiple queries

--- tests/testthat/test-SoilDataViewer.R
+++ tests/testthat/test-SoilDataViewer.R
@@ -3,12 +3,24 @@ test_that("get_SDV_legend_elements works", {
   
   skip_on_cran()
   
+  # simplify=TRUE, one where clause returning one legend
   res <- get_SDV_legend_elements("attributekey = 427")
   
   expect_true(inherits(res, 'data.frame'))
   
-  res <- get_SDV_legend_elements(c("attributekey = 427", 
+  # simplify=TRUE, two where clauses returning list of length 3 = 2 + 1 legends
+  res <- get_SDV_legend_elements(c("attributekey IN (427, 428)", 
                                    "nasisrulename = 'DHS - Potential for Radioactive Bioaccumulation'"))
   
-  expect_true(inherits(res, 'list'))
+  # one list, each element is a different legend
+  expect_equal(length(res), 3)
+  
+  # simplify=FALSE, two where clauses returning list of length two
+  res <- get_SDV_legend_elements(c("attributekey IN (427, 428)", 
+                                   "nasisrulename = 'DHS - Potential for Radioactive Bioaccumulation'"), 
+                                 simplify = FALSE)
+  
+  # one list for each WHERE clause
+  expect_equal(length(res), 2)
+  
 })
