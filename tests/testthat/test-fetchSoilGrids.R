d4995ce99306e1b95ce2d4a99d2502be717b58e3
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-04-25 11:20:46 -0700

    wrong expectation

--- tests/testthat/test-fetchSoilGrids.R
+++ tests/testthat/test-fetchSoilGrids.R
@@ -55,5 +55,6 @@ test_that("fetchSoilGrids: POINT input", {
                             stringsAsFactors = FALSE)
   your.points <- sf::st_as_sf(your.points, coords = c('lon','lat'))
   res <- try(fetchSoilGrids(your.points), silent = TRUE)
-  expect_true(inherits(res, 'data.frame'))
+  
+  expect_true(inherits(res, 'SoilProfileCollection'))
 })
