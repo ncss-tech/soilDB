bce1bf078fd15a590dfc31cf50ec744b8b1e95a8
Andrew Brown <andrew.g.brown@usda.gov>
2022-05-02 19:45:40 -0700

    get_SDA_cosurfmorph: fix CONCAT

--- R/get_SDA_cosurfmorph.R
+++ R/get_SDA_cosurfmorph.R
@@ -2,8 +2,8 @@
 ##  these queries are not very efficient because of the use of WHERE UPPER(compname) IN ", in.statement, "doesn't properly utilize existing indexes
 ##  related issue: # https://github.com/ncss-tech/sharpshootR/issues/12
 
-#' Get Geomorphic/Surface Morphometry Data from Soil Data Access 
-#' 
+#' Get Geomorphic/Surface Morphometry Data from Soil Data Access
+#'
 #' Get Geomorphic/Surface Morphometry Data from Soil Data Access or a local SSURGO data source and summarize by counts and proportions ("probabilities").
 #'
 #' @param table Target table to summarize. Default: `"cosurfmorphgc"` (3D Geomorphic Component). Alternate choices include `cosurfmorphhpp` (2D Hillslope Position), `cosurfmorphss` (Surface Shape), and  `cosurfmorphmr` (Microrelief).
@@ -15,15 +15,15 @@
 #' @param dsn Path to local SSURGO database SQLite database. Default `NULL` uses Soil Data Access.
 #' @param query_string Return query instead of sending to Soil Data Access / local database. Default: `FALSE`.
 #'
-#' @return a `data.frame` containing the grouping variable (`by`) and tabular summaries of counts and proportions of geomorphic records. 
-#' @details Default `table="cosurfmorphgc"` summarizes columns `geomposmntn`, `geomposhill`, `geomposflats`, and `geompostrce`.  
+#' @return a `data.frame` containing the grouping variable (`by`) and tabular summaries of counts and proportions of geomorphic records.
+#' @details Default `table="cosurfmorphgc"` summarizes columns `geomposmntn`, `geomposhill`, `geomposflats`, and `geompostrce`.
 #'          `table="cosurfmorphhpp"` summarizes `"hillslopeprof"`,  `table="cosurfmorphss"` summarizes `shapeacross` and `shapedown`, and `table="cosurfmorphmr"` summarizes `geomicrorelief`.
-#' 
+#'
 #'  Queries are a generalization of now-deprecated functions from {sharpshootR} by Dylan Beaudette: `geomPosMountainProbability()`, `geomPosHillProbability()`, `surfaceShapeProbability()`, `hillslopeProbability()`
-#'  
+#'
 #'  Similar summaries of SSURGO component surface morphometry data by series name can be found in `fetchOSD(, extended=TRUE)` or downloaded from \url{https://github.com/ncss-tech/SoilWeb-data}
 #'  Full component data including surface morphometry summaries at the "site" level can be obtained with `fetchSDA()`.
-#'  
+#'
 #' @seealso `fetchSDA()` `get_SDA_pmgroupname()`
 #' @export
 #' @author Dylan E. Beaudette, Andrew G. Brown
@@ -31,16 +31,16 @@
 #' \dontrun{
 #'  # Summarize by 3D geomorphic components by component name (default `by='compname'`)
 #'  get_SDA_cosurfmorph(WHERE = "areasymbol = 'CA630'")
-#'  
+#'
 #'  # Whole Soil Survey Area summary (using `by = 'areasymbol'`)
 #'  get_SDA_cosurfmorph(by = 'areasymbol', WHERE = "areasymbol = 'CA630'")
-#'  
+#'
 #'  # 2D Hillslope Position summary(using `table = 'cosurfmorphhpp'`)
 #'  get_SDA_cosurfmorph('cosurfmorphhpp', WHERE = "areasymbol = 'CA630'")
-#'  
+#'
 #'  # Surface Shape summary (using `table = 'cosurfmorphss'`)
 #'  get_SDA_cosurfmorph('cosurfmorphss', WHERE = "areasymbol = 'CA630'")
-#'  
+#'
 #'  # Microrelief summary (using `table = 'cosurfmorphmr'`)
 #'  get_SDA_cosurfmorph('cosurfmorphmr', WHERE = "areasymbol = 'CA630'")
 #' }
@@ -56,60 +56,63 @@ get_SDA_cosurfmorph <- function(table = c("cosurfmorphgc", "cosurfmorphhpp", "co
   if (is.null(mukeys) && is.null(areasymbols) && is.null(WHERE)) {
     stop("Please specify one of the following arguments: mukeys, areasymbols, WHERE", call. = FALSE)
   }
-  
+
   if (!is.null(mukeys)) {
     WHERE <- paste("mapunit.mukey IN", format_SQL_in_statement(as.integer(mukeys)))
   } else if (!is.null(areasymbols)) {
     WHERE <- paste("legend.areasymbol IN", format_SQL_in_statement(areasymbols))
-  } 
-  
+  }
+
   db <- match.arg(toupper(db), choices = c('SSURGO', 'STATSGO'))
   table <- match.arg(tolower(table), choices = c("cosurfmorphgc", "cosurfmorphhpp", "cosurfmorphss", "cosurfmorphmr"))
   statsgo_filter <- switch(db, SSURGO = "!=", STATSGO = "=")
-  
+
   vars <- switch(table,
                  "cosurfmorphgc" = c("geomposmntn", "geomposhill", "geomposflats", "geompostrce"),
                  "cosurfmorphhpp" = "hillslopeprof",
                  "cosurfmorphss" = c("shapeacross", "shapedown", "surfaceshape"),
                  # NOTE: surfaceshape is calculated CONCAT(shapeacross, '/', shapedown)
-                 "cosurfmorphmr" = "geomicrorelief") 
-                 
+                 "cosurfmorphmr" = "geomicrorelief")
+
   .SELECT_STATEMENT0 <- function(v) {
     paste0(paste0(v, ", ", paste0(v, "_n"), ", ", paste0(paste0("round(", v, "_n / total, 2) AS p_", v)), collapse = ", "))
   }
-  
+
   .SELECT_STATEMENT1 <- function(v) {
     res <- paste0(paste0(v, ", ", paste0(paste0("CAST(COUNT(", v, ") AS numeric) AS ", v, "_n")), collapse = ", "))
 
     # custom calculated column `surfaceshape` and `surfaceshape_n`
     if (table == "cosurfmorphss") {
-      res <- paste0("CONCAT(shapeacross, '/', shapedown) AS surfaceshape, 
+      res <- paste0("CONCAT(shapeacross, '/', shapedown) AS surfaceshape,
                      CAST(COUNT(CONCAT(shapeacross, '/', shapedown)) AS numeric) AS surfaceshape_n, ", res)
+      if (!is.null(dsn)) {
+        res <- gsub("CONCAT(shapeacross, '/', shapedown)", "shapeacross || '/' || shapedown", res, fixed = TRUE)
+      }
     }
-    
+
     res
   }
-  
+
   .JOIN_TABLE <- function(x) {
     sprintf("LEFT JOIN %s ON cogeomordesc.cogeomdkey = %s.cogeomdkey", x, x)
   }
-  
+
   .NULL_FILTER <- function(v) {
     paste0(paste0(v, collapse = " IS NOT NULL OR "), " IS NOT NULL")
   }
-  
+
   .ORDER_COLUMNS <- function(v) {
     paste0(paste0(paste0("p_", v), collapse = " DESC, "), " DESC")
   }
-  
+
   # excludes custom calculated columns (e.g. surfaceshape concatenated from across/down)
   vars_default <- vars[!grepl("surfaceshape", vars)]
-  
-  q <- paste0("SELECT a.[BYVAR], 
+
+  q <- paste0("SELECT a.[BYVAR],
                  ", .SELECT_STATEMENT0(vars), ",
                  total
                FROM (
-                 SELECT [BYVAR], [BYVAR] AS BYVAR, 
+                 SELECT [BYVAR], [BYVAR] AS BYVAR,
                  ", .SELECT_STATEMENT1(vars_default), "
                  FROM legend
                    INNER JOIN mapunit ON mapunit.lkey = legend.lkey
@@ -132,10 +135,10 @@ get_SDA_cosurfmorph <- function(table = c("cosurfmorphgc", "cosurfmorphhpp", "co
                  GROUP BY [BYVAR]) AS b
                ON a.BYVAR = b.BYVAR
                ORDER BY [BYVAR], ", .ORDER_COLUMNS(vars_default))
-  
+
   # insert grouping variable
   qsub <- gsub("[BYVAR]", by, q, fixed = TRUE)
-  
+
   if (query_string) {
     return(qsub)
   }
@@ -156,6 +159,6 @@ get_SDA_cosurfmorph <- function(table = c("cosurfmorphgc", "cosurfmorphhpp", "co
     # otherwise query from SDA
     res <- SDA_query(qsub)
   }
-  
+
   res
 }
