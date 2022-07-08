026d07fd7614298b82413705ea4bb5db63ed31b5
Andrew G. Brown <andrew.g.brown@usda.gov>
2022-07-08 09:38:41 -0700

    createSSURGO: update docs, expose `quiet` and implement `include_spatial` arguments

--- R/createSSURGO.R
+++ R/createSSURGO.R
@@ -56,7 +56,7 @@ downloadSSURGO <- function(WHERE = NULL,
   for (i in seq_along(urls)) {
     destfile <- file.path(destdir, basename(urls[i]))
     if (!file.exists(destfile)) {
-      try(download.file(urls[i], destfile = destfile, quiet = quiet, mode = "wb"))
+      try(download.file(urls[i], destfile = destfile, quiet = quiet, mode = "wb"), silent = quiet)
     }
   }
   
@@ -96,9 +96,11 @@ downloadSSURGO <- function(WHERE = NULL,
 #'
 #' @param filename Output file name (e.g. `'db.sqlite'` or `'db.gpkg'`)
 #' @param exdir Path containing containing SSURGO spatial (.shp) and tabular (.txt) files. 
-#' @param pattern Character. Optional regular expression to use to filter subdirectories of `exdir`. Default: `NULL` will search all subdirectories for SSURGO export files.#' @param include_spatial Logical. Include spatial data layers in database? Default: `TRUE`. 
+#' @param pattern Character. Optional regular expression to use to filter subdirectories of `exdir`. Default: `NULL` will search all subdirectories for SSURGO export files.
+#' @param include_spatial Logical. Include spatial data layers in database? Default: `TRUE`. 
 #' @param overwrite Logical. Overwrite existing layers? Default `FALSE` will append to existing tables/layers.
 #' @param header Logical. Passed to `read.delim()` for reading pipe-delimited (`|`) text files containing tabular data.
+#' @param quiet Logical. Suppress messages and other output from database read/write operations?
 #' @param ... Additional arguments passed to `write_sf()` for writing spatial layers.
 #'
 #' @return Character. Vector of layer/table names in `filename`.
@@ -108,7 +110,14 @@ downloadSSURGO <- function(WHERE = NULL,
 #'  downloadSSURGO("areasymbol IN ('CA067', 'CA077', 'CA632')", destdir = "SSURGO_test")
 #'  createSSURGO("test.gpkg", "SSURGO_test")
 #' }
-createSSURGO <- function(filename, exdir, pattern = NULL, overwrite = FALSE, header = FALSE, ...) {
+createSSURGO <- function(filename,
+                         exdir,
+                         pattern = NULL,
+                         include_spatial = TRUE,
+                         overwrite = FALSE,
+                         header = FALSE,
+                         quiet = TRUE,
+                         ...) {
   f <- list.files(exdir, recursive = TRUE, pattern = pattern, full.names = TRUE)
   
   if (!requireNamespace("sf"))
@@ -121,7 +130,7 @@ createSSURGO <- function(filename, exdir, pattern = NULL, overwrite = FALSE, hea
   #   "soilmu_a", "soilmu_l", "soilmu_p", "soilsa_a", "soilsf_l", "soilsf_p" 
   f.shp <- f[grepl(".*\\.shp$", f)]
   shp.grp <- do.call('rbind', strsplit(gsub(".*soil([musfa]{2})_([apl])_([a-z]{2}\\d{3})\\.shp", "\\1;\\2;\\3", f.shp), ";", fixed = TRUE))
-  if (length(shp.grp) > 1) {
+  if (length(shp.grp) > 1 && include_spatial) {
     f.shp.grp <- split(f.shp, list(feature = shp.grp[,1], geom = shp.grp[,2]))
     
     lapply(seq_along(f.shp.grp), function(i) {
@@ -193,7 +202,7 @@ createSSURGO <- function(filename, exdir, pattern = NULL, overwrite = FALSE, hea
           }
         }
         y
-    }))), silent = TRUE)
+    }))), silent = quiet)
     
     if (length(mstab_lut[x]) && is.na(mstab_lut[x])) {
       mstab_lut[x] <- x
@@ -210,14 +219,14 @@ createSSURGO <- function(filename, exdir, pattern = NULL, overwrite = FALSE, hea
         if (overwrite) {
           RSQLite::dbWriteTable(con, mstab_lut[x], d, overwrite = TRUE)
         } else RSQLite::dbWriteTable(con, mstab_lut[x], d, append = TRUE)
-      }, silent = FALSE)
+      }, silent = quiet)
       
       # create pkey indices
       if (!is.null(indexPK) && length(indexPK) > 0) {
         try({
           RSQLite::dbExecute(con, sprintf("CREATE UNIQUE INDEX IF NOT EXISTS %s ON %s (%s)", 
                                           paste0('PK_', mstab_lut[x]), mstab_lut[x], paste0(indexPK, collapse = ",")))
-        }, silent = FALSE)
+        }, silent = quiet)
       }
       
       # TODO: other foreign keys/relationships? ALTER TABLE/ADD CONSTRAINT not available in SQLite
