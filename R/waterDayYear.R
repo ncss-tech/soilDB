7ebf5bc853e32de43b6e99a40978ca1d853ef0c6
Andrew Brown <andrew.g.brown@usda.gov>
2022-10-12 01:44:25 -0300

    waterDayYear: fix for #268 + cleanup + new tests

--- R/waterDayYear.R
+++ R/waterDayYear.R
@@ -6,16 +6,16 @@
 
 
 #' Compute Water Day and Year
-#' 
+#'
 #' Compute "water" day and year, based on the end of the typical or legal dry
 #' season. This is September 30 in California.
-#' 
+#'
 #' This function doesn't know about leap-years. Probably worth checking.
-#' 
+#'
 #' @param d anything the can be safely converted to \code{POSIXlt}
 #' @param end "MM-DD" notation for end of water year
 #' @param format Used in POSIXlt conversion. Default `"%Y-%m-%d"`
-#' @param tz Used in POSIXlt conversion for custom timezone. Default `""` is current locale 
+#' @param tz Used in POSIXlt conversion for custom timezone. Default is `"UTC"`
 #' @return A \code{data.frame} object with the following \item{wy}{the "water
 #' year"} \item{wd}{the "water day"}
 #' @author D.E. Beaudette
@@ -24,36 +24,39 @@
 #' \url{https://stackoverflow.com/questions/48123049/create-day-index-based-on-water-year}
 #' @keywords manip
 #' @examples
-#' 
+#'
 #' # try it
 #' waterDayYear('2019-01-01')
-#' 
+#'
 #' @export waterDayYear
-waterDayYear <- function(d, end = "09-30", format = "%Y-%m-%d", tz = "") {
-  
-  # note: tz = "" will assume the current / LOCALE-specific timezone
-  # convert to water year, using Sept
-  # ideas from: https://github.com/USGS-R/dataRetrieval/issues/246
-  dLT <- as.POSIXlt(d, format = format, tz = tz)
-  
+waterDayYear <- function(d, end = "09-30", format = "%Y-%m-%d", tz = "UTC") {
+
+  # create a date+timezone (and possibly time) from input
+  dLT <- as.POSIXlt(as.Date(d, format = format), tz = tz)
+
   # add century from epoch
   water_year <- dLT$year + 1900
-  
-  # water year starts in September of prior year
-  water_year[which(dLT$mon >= 9)] <- water_year[which(dLT$mon >= 9)] + 1
-  
-  # dates of prior water year start
-  lastWY <- as.Date(sprintf("%s-%s", water_year - 1, end), format = "%Y-%m-%d", tz = tz)
-  
+
+  # extract julian day the new water year starts
+  startDay <- as.POSIXlt(as.Date(end[1], format = "%m-%d") + 1, tz = "UTC")$yday
+
+  # water year starts day after `end` of _prior_ year,
+  # so increment calendar year for days after `end`
+  wyidx <- which(dLT$yday >= startDay)
+  water_year[wyidx] <- water_year[wyidx] + 1
+
+  # date of the last day of the actual water year
+  lastWY <- as.POSIXlt(as.Date(sprintf("%s-%s", water_year - 1, end), format = "%Y-%m-%d"), tz = tz)
+
   # compute water "day" as days since start of water year
-  # ideas from: https://stackoverflow.com/questions/48123049/create-day-index-based-on-water-year
-  water_day <- as.integer(difftime(dLT, lastWY, units = 'days'))
-  
+  # NOTE: round()ing b/c as.integer(difftime(..., units='days')) is like floor()
+  water_day <- as.integer(round(difftime(dLT, lastWY, units = 'days')))
+
   res <- data.frame(
-    wy = water_year, 
+    wy = water_year,
     wd = water_day
   )
-  
+
   return(res)
 }
 
--- tests/testthat/test-waterDayYear.R
+++ tests/testthat/test-waterDayYear.R
@@ -2,7 +2,9 @@ context("water year/day")
 
 
 test_that("works as expected", {
-  
+
+  # NOTE: waterDayYear requires `tz` argument for consistent conversion across locales
+
   # 2008 is a leap year
   res.lr <- waterDayYear(as.Date("2008-03-01"))
   res <- waterDayYear(as.Date("2009-03-01"))
@@ -10,44 +12,86 @@ test_that("works as expected", {
   # check years
   expect_equal(res.lr$wy, 2008L)
   expect_equal(res$wy, 2009L)
-  
+
   # days are offset by 1
   expect_equal(res.lr$wd, 153L)
   expect_equal(res$wd, 152L)
-  
+
   # first day of water year 2019
   x <- as.Date("2018-10-01")
   res <- waterDayYear(x)
-  
+
   expect_equal(res$wy, 2019)
   expect_equal(res$wd, 1)
-  
+
   # last day of water year 2018
   x <- as.Date("2018-09-30")
   res <- waterDayYear(x)
-  
+
   expect_equal(res$wy, 2018)
   expect_equal(res$wd, 365)
-  
+
   # important dates
   x <- as.Date(c("1982-11-19", "1981-02-17"))
   res <- waterDayYear(x)
-  
+
   # TODO: double-check these
   expect_equal(res$wy, c(1983, 1981))
   expect_equal(res$wd, c(50,  140))
-  
+
   # structural integrity
   expect_true(inherits(res, 'data.frame'))
   expect_true(length(x) == nrow(res))
-  
+
   # text interface, with YYYY-MM-DD hh:mm:ss
-  # NOTE: waterDayYear and .formatDates involving hh:mm:ss in tests 
-  #       require `format` and `tz` argument for consistent conversion across locales
+  # NOTE: waterDayYear requires `format` argument for subdaily accuracy
   res <- waterDayYear("2000-12-05 12:00:00", format = "%Y-%m-%d %H:%M:%S", tz = "UTC")
   expect_equal(res$wy, 2001L)
-  
+
   expect_equal(res$wd, 66L)
-  
+
 })
 
+test_that("non-standard `end` of water year", {
+  x <- as.Date("2018-09-30")
+
+  # water year "2019" starts hypothetically on March 11th, 2018
+  res <- waterDayYear(x, end = '03-10')
+
+  expect_equal(res$wy, 2019L)
+  expect_equal(res$wd, as.integer(round(difftime("2018-09-30", "2018-03-10"))))
+})
+
+.waterDayYearTZTest <- function(x, tz, test_tz = "UTC", ...) {
+  tzorig <- Sys.getenv("TZ")
+  Sys.setenv(TZ = tz)
+  x <- waterDayYear(x, tz = test_tz, ...)
+  Sys.setenv(TZ = tzorig)
+  x$wd
+}
+
+test_that("waterDayYear gives consistent result within a timezone", {
+  # test waterDayYear in other timezones
+  res <- sapply(OlsonNames(),
+              function(x)
+                .waterDayYearTZTest("1992-09-11", tz = x, test_tz = x))
+  expect_true(all(res == 347))
+
+  res <- sapply(OlsonNames(),
+                function(x)
+                  .waterDayYearTZTest("2000-12-05 12:00:00", tz = x, test_tz = x, format = "%Y-%m-%d %H:%M:%S"))
+  expect_true(all(res == 66))
+})
+
+test_that("waterDayYear tz='UTC' behaves the same across timezones" , {
+  # test waterDayYear behavior with constant (UTC) timezone in simulated other timezones
+  res <- sapply(OlsonNames(),
+                function(x)
+                  .waterDayYearTZTest("1992-09-11", tz = x, test_tz = "UTC"))
+  expect_true(all(res == 347))
+
+  res <- sapply(OlsonNames(),
+              function(x)
+                .waterDayYearTZTest("2000-12-05 12:00:00", tz = x, test_tz = "UTC", format = "%Y-%m-%d %H:%M:%S"))
+  expect_true(all(res == 66))
+})
