40bcb56aadba189b57dbd794a26e1b1ddc0db989
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-01-12 13:21:04 -0800

    Small updates to `fetchNASIS()` docs

--- R/fetchNASIS.R
+++ R/fetchNASIS.R
@@ -1,22 +1,16 @@
-# convenient interface to local NASIS data
-# from: pedons | components | lab | ???
-# ... : arguments passed on to helper functions
-
-
 #' Get a pedon or component data SoilProfileCollection from NASIS
 #'
-#' Fetch commonly used site/pedon/horizon data or component from NASIS,
+#' Fetch commonly used site/pedon/horizon or mapunit component data from NASIS,
 #' returned as a SoilProfileCollection object.
 #'
-#' This function imports data from NASIS into R as a
-#' \code{SoilProfileCollection} object. It "flattens" NASIS pedon and component
-#' tables, including their child tables, into several more easily manageable
-#' data frames. Primarily these functions access the local NASIS database using
-#' an ODBC connection. However using the \code{fetchNASIS()} argument
-#' \code{from = "pedon_report"}, data can be read from the NASIS Report
-#' 'fetchNASIS', as either a txt file or url. The primary purpose of
-#' \code{fetchNASIS(from = "pedon_report")} is to facilitate importing datasets
-#' larger than 8000+ pedons/components.
+#' This function imports data from NASIS into R as a \code{SoilProfileCollection} object.
+#' It "flattens" NASIS pedon and component tables, including their child tables, into several 
+#' more manageable data frames. Primarily these functions access the local NASIS database using
+#' an ODBC connection. The `dsn` argument allows you to specify a path or `DBIConnection`
+#' to an SQLite database. The argument \code{from = "pedon_report"}, data can be read 
+#' from the NASIS Report 'fetchNASIS', from either text file or URL (specified as `url`). The primary purpose
+#' of \code{fetchNASIS(from = "pedon_report")} is importing datasets larger than 8000+ 
+#' pedons/components.
 #'
 #' The value of \code{nullFragsAreZero} will have a significant impact on the
 #' rock fragment fractions returned by fetchNASIS. Set \code{nullFragsAreZero =
@@ -26,15 +20,15 @@
 #' fragment values should be interpreted as 0.
 #'
 #' This function attempts to do most of the boilerplate work when extracting
-#' site/pedon/horizon or component data from a local NASIS database. Pedons
-#' that are missing horizon data, or have errors in their horizonation are
-#' excluded from the returned object, however, their IDs are printed on the
+#' site/pedon/horizon or component data from a local NASIS database. Pedon IDs
+#' that are missing horizon data, or have errors in their horizonation are printed on the
 #' console. Pedons with combination horizons (e.g. B/C) are erroneously marked
 #' as errors due to the way in which they are stored in NASIS as two
 #' overlapping horizon records.
 #'
 #' Tutorials:
 #'
+#'  - [fetchNASIS Columns](http://ncss-tech.github.io/soilDB/articles/fetchNASIS.html)
 #'  - [fetchNASIS Pedons Tutorial](http://ncss-tech.github.io/AQP/soilDB/fetchNASIS-mini-tutorial.html)
 #'  - [fetchNASIS Components Tutorial](http://ncss-tech.github.io/AQP/soilDB/NASIS-component-data.html)
 #'
@@ -42,7 +36,7 @@
 #' get_phfmp_from_NASIS_db get_RMF_from_NASIS_db
 #' get_concentrations_from_NASIS_db
 #' 
-#' @param from determines what objects should fetched? ('pedons' | 'components' | 'pedon_report')
+#' @param from determines what objects should fetched? Default: `'pedons'`. Alternately, `'components'`, or `'pedon_report'`.
 #' @param url string specifying the url for the NASIS pedon_report (default:
 #' `NULL`)
 #' @param SS fetch data from the currently loaded selected set in NASIS or from
@@ -53,8 +47,7 @@
 #' (default: `TRUE`), see details
 #' @param soilColorState Used only for `from='pedons'`; which colors should be used to generate the convenience field `soil_color`? (`'moist'` or `'dry'`)
 #' @param mixColors should mixed colors be calculated (Default: `TRUE`) where multiple colors are populated for the same moisture state in a horizon? `FALSE` takes the dominant color for each horizon moist/dry state.
-#' @param lab should the `phlabresults` child table be fetched with
-#' site/pedon/horizon data (default: `FALSE`)
+#' @param lab should the `phlabresults` child table be fetched with site/pedon/horizon data (default: `FALSE`)
 #' @param fill include pedon or component records without horizon data in result? (default: `FALSE`)
 #' @param dropAdditional Used only for `from='components'` with `duplicates=TRUE`. Prevent "duplication" of `mustatus=="additional"`  mapunits? Default: `TRUE`
 #' @param dropNonRepresentative Used only for `from='components'` with `duplicates=TRUE`. Prevent "duplication" of non-representative data mapunits? Default: `TRUE`
@@ -87,14 +80,6 @@ fetchNASIS <- function(from = 'pedons',
     .Deprecated(msg = sprintf("stringsAsFactors argument is deprecated.\nSetting package option with `NASISDomainsAsFactor(%s)`", stringsAsFactors))
     NASISDomainsAsFactor(stringsAsFactors)
   }
-  
-  # TODO: do we need _View_1 tables in the sqlite table snapshot? Could be handy for
-  #       specialized selected sets crafted by NASIS/CVIR stuff; currently you are allowed
-  #       to specify the selected set for a SQLite database, and I suppose the convention
-  #       should be for those tables to be there, even if empty
-
-  # if (!is.null(dsn))
-  #   SS <- FALSE
 
   # sanity check
   if (!from %in% c('pedons', 'components', 'pedon_report')) {
