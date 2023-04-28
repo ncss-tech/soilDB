0ab7663f6b7ff163be2e657611bc98453793f426
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-04-28 12:11:50 -0700

    Add tests for .make_WSS_download_url() for #286 (2)

---
+++ tests/testthat/test-get_SSURGO_utils.R
@@ -0,0 +1,13 @@
+test_that(".make_WSS_download_url works", {
+  skip_if_offline()
+  
+  skip_on_cran()
+  
+  x <- .make_WSS_download_url("areasymbol IN ('NH607', 'VT005', 'VT009', 'VT019')")
+  expect_equal(length(x), 4)
+  
+  # vermont has a state-specific template
+  x <- .make_WSS_download_url("areasymbol IN ('NH607', 'VT005', 'VT009', 'VT019')", include_template = TRUE)
+  expect_equal(grepl("_soildb_US_", x), c(TRUE, FALSE, FALSE, FALSE))
+  
+})
