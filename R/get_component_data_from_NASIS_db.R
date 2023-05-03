af4280f4a2b6b496facadcd90684fe59aaa59d9e
Andrew G. Brown <andrew.g.brown@usda.gov>
2023-05-03 09:33:15 -0700

    get_component_data_from_NASIS_db: remove duplicated hydricrating column, include unaliased climate columns

--- R/get_component_data_from_NASIS_db.R
+++ R/get_component_data_from_NASIS_db.R
@@ -41,10 +41,18 @@ get_component_data_from_NASIS_db <- function(SS = TRUE,
     NASISDomainsAsFactor(stringsAsFactors)
   }
   
-  q1 <- "SELECT dmudesc, compname, comppct_r, compkind, majcompflag, localphase, drainagecl, hydricrating, elev_l, elev_r, elev_h, slope_l, slope_r, slope_h, aspectccwise, aspectrep, aspectcwise, map_l, map_r, map_h, airtempa_l as maat_l, airtempa_r as maat_r, airtempa_h as maat_h, soiltempa_r as mast_r, reannualprecip_r, ffd_l, ffd_r, ffd_h, tfact, wei, weg, nirrcapcl, nirrcapscl, nirrcapunit, irrcapcl, irrcapscl, irrcapunit, frostact, hydricrating, hydgrp, corcon, corsteel, taxclname, taxorder, taxsuborder, taxgrtgroup, taxsubgrp, taxpartsize, taxpartsizemod, taxceactcl, taxreaction, taxtempcl, taxmoistscl, taxtempregime, soiltaxedition, coiid, dmuiid
-
-  FROM
-  datamapunit_View_1 AS dmu
+  q1 <- "SELECT dmudesc, compname, comppct_r, compkind, majcompflag, 
+                localphase, drainagecl, hydricrating, hydgrp, tfact, wei, weg, frostact, corcon, corsteel, 
+                elev_l, elev_r, elev_h, slope_l, slope_r, slope_h, 
+                aspectccwise, aspectrep, aspectcwise, map_l, map_r, map_h, 
+                airtempa_l, airtempa_r, airtempa_h, soiltempa_l, soiltempa_r, soiltempa_h,
+                airtempa_l as maat_l, airtempa_r as maat_r, airtempa_h as maat_h, soiltempa_r as mast_r, 
+                reannualprecip_r, ffd_l, ffd_r, ffd_h,
+                nirrcapcl,  nirrcapscl, nirrcapunit, irrcapcl, irrcapscl, irrcapunit, 
+                taxclname, taxorder, taxsuborder, taxgrtgroup, taxsubgrp, 
+                taxpartsize, taxpartsizemod, taxceactcl, taxreaction, taxtempcl, 
+                taxmoistscl, taxtempregime, soiltaxedition, coiid, dmuiid
+  FROM datamapunit_View_1 AS dmu
   INNER JOIN component_View_1 AS co ON co.dmuiidref = dmu.dmuiid
 
   ORDER BY dmudesc, comppct_r DESC, compname ASC;"
