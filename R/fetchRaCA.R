34807ed3db95c05da47b6f4830c50509e4842b60
Andrew Gene Brown <andrew.g.brown@usda.gov>
2023-12-20 16:31:55 -0800

    Deprecate `fetchRaCA()`  - closes #249

--- R/fetchRaCA.R
+++ R/fetchRaCA.R
@@ -1,6 +1,10 @@
 #' @title Get Rapid Carbon Assessment (RaCA) data
 #' 
-#' @description Get Rapid Carbon Assessment (RaCA) data by state, geographic bounding-box, RaCA site ID, or soil series query from the SoilWeb API. This interface to the data was an experimental delivery service that does not include the latest soil organic carbon (SOC) measurements. 
+#' @description 
+#' 
+#' **NOTICE:** The SoilWeb snapshot of the RaCA data has been deprecated. The latest version of the data, including values measured by the Kellogg Soil Survey Laboratory, and supporting documentation, are available here: <https://www.nrcs.usda.gov/resources/data-and-reports/rapid-carbon-assessment-raca>. Download link on Box.com: <https://nrcs.app.box.com/s/upx5xhlwis7saunfiysclfrhl5vxxudn>
+#' 
+#' Get Rapid Carbon Assessment (RaCA) data by state, geographic bounding-box, RaCA site ID, or soil series query from the SoilWeb API. This interface to the data was an experimental delivery service that does not include the latest soil organic carbon (SOC) measurements. 
 #' 
 #' Please use [current RaCA distribution](https://data.nal.usda.gov/dataset/rapid-carbon-assessment-raca) if you need lab _measured_ SOC rather than SOC estimated by VNIR.
 #' 
@@ -36,7 +40,8 @@
 #' @export
 fetchRaCA <- function(series=NULL, bbox=NULL, state=NULL, rcasiteid=NULL, get.vnir=FALSE) {
 
-  # TODO: CY2022 https://github.com/ncss-tech/soilDB/issues/249
+  # see https://github.com/ncss-tech/soilDB/issues/249
+  .Deprecated(msg = "The SoilWeb snapshot of the RaCA data has been deprecated. The latest version of the data, including values measured by the Kellogg Soil Survey Laboratory, and supporting documentation, are available here: <https://www.nrcs.usda.gov/resources/data-and-reports/rapid-carbon-assessment-raca>. Download link on Box.com: <https://nrcs.app.box.com/s/upx5xhlwis7saunfiysclfrhl5vxxudn>")
   
   # important: change the default behavior of data.frame
   opt.original <- options(stringsAsFactors = FALSE)
