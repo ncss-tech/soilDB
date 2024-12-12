71cb072f7a8e46c1f285269997f1db7e39e5fff7
Andrew Gene Brown <andrew.g.brown@usda.gov>
2024-12-12 08:28:52 -0800

    `fetchNASIS("pedons")`: add test of surface and subsurface fragments derived using `simplifyFragmentData()`

--- tests/testthat/test-soilDBdata.R
+++ tests/testthat/test-soilDBdata.R
@@ -10,6 +10,7 @@ test_that("soilDBdata: fetchNASIS", {
 
   # test SS=FALSE
   fp <- fetchNASIS(from = "pedons", dsn = PEDON_TEST_DSN, SS = FALSE)
+  
   fc <- fetchNASIS(from = "components", dsn = COMPONENT_TEST_DSN, SS = FALSE)
 
   # TODO: test SS=TRUE
@@ -24,6 +25,17 @@ test_that("soilDBdata: fetchNASIS", {
       fill = TRUE
     )
   })
+  
+  p1 <- aqp::subset(fpf, peiid == 1092666)
+  
+  # check surface fragments
+  expect_equal(p1$surface_stones, 2.4)
+  expect_equal(p1$surface_total_frags_pct, 5.4)
+  
+  # check subsurface fragments 
+  expect_equal(p1$stones, rep(5, 4))
+  expect_equal(p1$total_frags_pct, c(90, 75, 65, 75))
+  
   expect_warning({
     fcf <- fetchNASIS(
       from = "components",
