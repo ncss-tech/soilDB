d1ff62f3f17551bbc1a90d9be3d56ad1d682941a
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-02-20 17:06:54 -0800

    move datasets back to /data/, unexport `aqp_data()`

--- R/aqp_data.R
+++ R/aqp_data.R
@@ -5,12 +5,12 @@
 #' @param ... Literal character strings or names.
 #' @param list character. A vector of object names to load.
 #' @param verbose logical. If `TRUE`, additional diagnostics are printed.
-#' @param envir The environment where the data should be loaded.
+#' @param envir environment. The environment where the data should be loaded. Default `.GlobalEnv`.
+#' @param path character. Path (within package `/inst/` folder) to data sets. Default: `"extdata"`
+#' @param package character. Package name to load data from. Default: `"soilDB"`
 #' @param overwrite logical. Should existing objects of the same name in `envir` be replaced? Default: `TRUE`
-#'
+#' @noRd
 #' @return Called for side-effect of loading the specified dataset into global environment.
-#' @export
-#'
 #' @examplesIf requireNamespace("aqp", quietly = TRUE)
 #' 
 #' aqp_data(loafercreek)
@@ -19,7 +19,12 @@ aqp_data <- function(...,
                      list = character(),
                      verbose = getOption("verbose"),
                      envir = .GlobalEnv,
+                     path = "extdata",
+                     package = "soilDB",
                      overwrite = TRUE) {
+  
+  # This is a draft function that would support moving SoilProfileCollection objects to inst/extdata
+  # In theory this would eliminate the need for soilDB to explicitly import the SPC class and the aqp package
   
   if (!requireNamespace("aqp")) {
     stop("package 'aqp' is required", call. = FALSE)
@@ -29,7 +34,7 @@ aqp_data <- function(...,
   nn <- c(as.character(substitute(list(...))[-1L]), list)
   
   for (n in nn) {
-    fn <- system.file("extdata", paste0(n, ".rda"), package = "soilDB")
+    fn <- system.file(path, paste0(n, ".rda"), package = package)
     if (file.exists(fn) && (overwrite || !exists(n, envir = envir))) {
       load(fn, envir = envir, verbose = verbose)
     } else {
