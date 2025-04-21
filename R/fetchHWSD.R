b3bc84b7332535d20274ac7388c214f9bdec2604
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-04-21 10:48:30 -0700

    use `soilDB_user_dir()`

--- R/fetchHWSD.R
+++ R/fetchHWSD.R
@@ -4,16 +4,17 @@
 #' information. Source raster map in ESRI Grid format (.bil) is converted to GeoTIFF.
 #' The source tabular database in Microsoft Access (.mdb) format is converted to SQLite.
 #' 
-#' @param x Extent of interest that is passed to `terra::crop()` `y` argument. A
-#'   SpatRaster, SpatVector, SpatExtent, or any other object that has a
-#'   SpatExtent. Default `NULL` returns the full dataset.
-#' @param hwsd_url URL for downloading HWSD dataset. Default
+#' @param x A _SpatRaster_, _SpatVector_, _SpatExtent_, or any other object that has
+#'   a _SpatExtent_. Default `NULL` returns the full dataset. Extent of interest
+#'   that is passed to `terra::crop()` `y` argument.
+#' @param hwsd_url _character_. URL for downloading HWSD dataset. Default
 #'   `"https://s3.eu-west-1.amazonaws.com/data.gaezdev.aws.fao.org/HWSD/"`
+#' @param hwsd_version _integer_. Default `2L`. 
 #' @param force Force rebuilding of cache. Default: `FALSE`
 #' @source Food and Agriculture Organization of the United Nations (FAO), Soils
 #'   Portal, Harmonized World Soil Database (HWSD) v2.0
 #'   \url{https://www.fao.org/soils-portal/data-hub/soil-maps-and-databases/harmonized-world-soil-database-v20/en/}
-#' @return A SpatRaster object with mapunit-level aggregate information stored
+#' @return A _SpatRaster_ object with mapunit-level aggregate information stored
 #'   as categories.
 #' @export
 #' @importFrom utils unzip
@@ -24,28 +25,27 @@
 #'   
 #'   # categorical data (WRB class)
 #'   terra::activeCat(res) <- "WRB4"
+#'   
+#'   # view WRB4 map
 #'   terra::plot(res)
+#'   terra::lines(x, col = "white")
 #'   
 #'   # convert categories containing numeric data to numeric values
 #'   res2 <- terra::catalyze(res)
 #'   
+#'   # view AWC map
 #'   terra::plot(res2$AWC, main = "Available Water Capacity, mm")
 #'   terra::lines(x, col = "white")
 #' }
 fetchHWSD <- function(x = NULL, 
                       hwsd_url = "https://s3.eu-west-1.amazonaws.com/data.gaezdev.aws.fao.org/HWSD/", 
+                      hwsd_version = 2L,
                       force = FALSE) {
-  
-  # handling for future versions (older versions not available)
-  HWSD_VERSION <- 2L
-  
-  data_dir <- file.path(tools::R_user_dir("soilDB", "data"), paste0("HWSD", HWSD_VERSION))
-  dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
-  
-  tiff_path <- file.path(data_dir, sprintf("HWSD%s.tif", HWSD_VERSION))
+  data_dir <- HWSD_path(hwsd_version)
+  tiff_path <- file.path(data_dir, sprintf("HWSD%s.tif", hwsd_version))
   if (isTRUE(force) || !file.exists(tiff_path)) {
     bil_url <- paste0(hwsd_url,
-                      sprintf("HWSD%s_RASTER.zip/HWSD%s.bil", HWSD_VERSION))
+                      sprintf("HWSD%s_RASTER.zip/HWSD%s.bil", hwsd_version))
     r <- terra::writeRaster(terra::rast(paste0("/vsizip/vsicurl/", bil_url)), 
                             tiff_path, 
                             datatype = "INT4U")
@@ -53,12 +53,12 @@ fetchHWSD <- function(x = NULL,
     r <- terra::rast(tiff_path)
   }
   
-  sqlite_path <- file.path(data_dir, "HWSD2.sqlite")
-  mdb_path <- file.path(data_dir, "HWSD2.mdb")
-  mdb_zip_path <- file.path(data_dir, "HWSD2_DB.zip")
+  sqlite_path <- file.path(data_dir, sprintf("HWSD%s.sqlite", hwsd_version))
+  mdb_path <- file.path(data_dir, sprintf("HWSD%s.mdb", hwsd_version))
+  mdb_zip_path <- file.path(data_dir, sprintf("HWSD%s_DB.zip", hwsd_version))
   
   if (isTRUE(force) || !file.exists(mdb_path)){
-    mdb_url <- paste0(hwsd_url, "HWSD2_DB.zip")
+    mdb_url <- paste0(hwsd_url, sprintf("HWSD%s_DB.zip", hwsd_version))
     curl::curl_download(
       mdb_url, 
       destfile = mdb_zip_path, 
@@ -83,14 +83,18 @@ fetchHWSD <- function(x = NULL,
     r2 <- r
   }
   
-  hwsd2_smu <- soilDB::SDA_query("SELECT * FROM HWSD2_SMU", sqlite_path)
-  levels(r2) <- hwsd2_smu[-1]
+  # add soil mapunit information to attribute table
+  hwsd_smu <- soilDB::SDA_query(sprintf("SELECT * FROM HWSD%s_SMU", hwsd_version), sqlite_path)
+  if (!inherits(hwsd_smu, 'try-error')) {
+    levels(r2) <- hwsd_smu[-1]
+  }
   r2
 }
 
 HWSD_path <- function(HWSD_VERSION = 2L) {
-  # TODO abstract this logic; see https://github.com/ncss-tech/soilDB/issues/377
-  data_dir <- file.path(tools::R_user_dir("soilDB", "data"), paste0("HWSD", HWSD_VERSION))
-  dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
-  file.path(data_dir, "HWSD%s.sqlite")
+  soilDB_user_dir("data", paste0("HWSD", HWSD_VERSION))
+}
+
+HWSD_db <- function(HWSD_VERSION = 2L) {
+  file.path(HWSD_path(HWSD_VERSION), sprintf("HWSD%s.sqlite", HWSD_VERSION))
 }
