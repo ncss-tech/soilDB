c0bd2808918f50a9593549681cd9cba2bca0eb15
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-06-02 13:58:21 -0700

    fetchLDM tests: `suppressWarnings()` rather than `expect_warning()`  - no warning is generated -> failure when SDA is down/returns try-error

--- tests/testthat/test-fetchLDM.R
+++ tests/testthat/test-fetchLDM.R
@@ -11,14 +11,13 @@ test_that("fetchLDM works", {
   library(aqp)
   
   # physical and chemical properties tables are 1:1 with lab_layer
-  res <- expect_warning(
+  res <- suppressWarnings(
     fetchLDM(
       "Typic Argialbolls",
       what = "corr_taxsubgrp",
       tables = c("lab_physical_properties", 
                  "lab_chemical_properties")
-    ),
-    "horizon bottom depths are shallower than top depth"
+    )
   )
   skip_if(inherits(res, 'try-error') || is.null(res))
   
@@ -26,7 +25,7 @@ test_that("fetchLDM works", {
   
   # xray/thermal table allows for different prep_code and analyzed_size_frac
   #  w/ default prep_code = "S" and analyzed_size_frac = "<2 mm"
-  res2 <- expect_warning(expect_message(
+  res2 <- suppressWarnings(expect_message(
     fetchLDM(
       "Typic Argialbolls",
       what = "corr_taxsubgrp",
@@ -36,8 +35,7 @@ test_that("fetchLDM works", {
         "lab_xray_and_thermal"
       )
     )
-  ),
-  "horizon bottom depths are shallower than top depth")
+  ))
   
   skip_if(inherits(res2, 'try-error') || is.null(res2))
   
@@ -47,7 +45,7 @@ test_that("fetchLDM works", {
   
   # specifying a size fraction will join in just that subset of data
   # so number of profiles and horizons matches that of the query without xray data
-  res3 <- expect_warning(
+  res3 <- suppressWarnings(
     fetchLDM(
       "Typic Argialbolls",
       what = "corr_taxsubgrp",
@@ -57,8 +55,7 @@ test_that("fetchLDM works", {
         "lab_xray_and_thermal"
       ),
       analyzed_size_frac = "0.02-0.05 mm"
-    ),
-    "horizon bottom depths are shallower than top depth"
+    )
   )
   
   skip_if(inherits(res3, 'try-error') || is.null(res3))
