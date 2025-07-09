bd705c86320c06949e05e1a5efbf19cf31cb543c
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-07-09 09:55:43 -0700

    Update NASIS metadata for data model v7.4.3

--- data-raw/NASIS_metadata.R
+++ data-raw/NASIS_metadata.R
@@ -1,5 +1,6 @@
 ## code to prepare `metadata` dataset goes here
 # make data/metadata.rda (used by uncode() when NASIS not available)
 
-metadata <- soilDB:::.get_NASIS_metadata()
+metadata <- soilDB::get_NASIS_metadata(include_description = TRUE)
 usethis::use_data(metadata, overwrite = TRUE, compress = 'xz')
+# View(metadata)
