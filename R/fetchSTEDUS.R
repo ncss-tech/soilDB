d4025f0beae7ad02227309480fb717ead1fad2b5
Andrew Gene Brown <andrew.g.brown@usda.gov>
2025-07-15 16:59:56 -0700

    bump

--- R/fetchSTEDUS.R
+++ R/fetchSTEDUS.R
@@ -1,21 +1,26 @@
 #' Fetch Standardized Elevation Derivatives of the United States (STEDUS) Grids
 #'
-#' This tool creates a virtual raster or downloads data for an extent from Cloud Optimized GeoTIFFs
-#' (COGs) from the Standardized Elevation Derivatives of the United States (STEDUS) project.
-#' 
-#' @param x  An R spatial object (such as a _SpatVector_, _SpatRaster_, or _sf_ object) or a
-#'   _SoilProfileCollection_ with coordinates initialized via `aqp::initSpatial<-`. Default: `NULL`
-#'   returns the CONUS extent as virtual raster. If `x` is a _SpatRaster_ the coordinate reference
-#'   system, extent, and resolution are used as a template for the output raster.
-#' @param variables One or more variables corresponding to STEDUS grid file names (without .tif
-#'   extension). See \url{https://storage.googleapis.com/dsm-ft-stedus-30m-public/index.html} for
-#'   details.
-#' @param filename character. Path to write output raster file. Default: `NULL` will keep result in
-#'   memory (or store in temporary file if memory threshold is exceeded)
-#' @param overwrite Overwrite `filename` if it exists? Default: `FALSE` 
+#' This tool creates a virtual raster or downloads data for an extent from Cloud
+#' Optimized GeoTIFFs (COGs) from the Standardized Elevation Derivatives of the
+#' United States (STEDUS) project.
 #'
-#' @references Brungard et al. (2024) 30 meter Standardized Elevation Derivatives of the United
-#'   States (STEDUS30). Manuscript in preparation. Available online:
+#' @param x  An R spatial object (such as a _SpatVector_, _SpatRaster_, or _sf_
+#'   object) or a _SoilProfileCollection_ with coordinates initialized via
+#'   `aqp::initSpatial<-`. Default: `NULL` returns the CONUS extent as virtual
+#'   raster. If `x` is a _SpatRaster_ the coordinate reference system, extent,
+#'   and resolution are used as a template for the output raster.
+#' @param variables One or more variables corresponding to STEDUS grid file
+#'   names (without .tif extension). See
+#'   \url{https://storage.googleapis.com/dsm-ft-stedus-30m-public/index.html}
+#'   for details.
+#' @param filename character. Path to write output raster file. Default: `NULL`
+#'   will keep result in memory (or store in temporary file if memory threshold
+#'   is exceeded)
+#' @param overwrite Overwrite `filename` if it exists? Default: `FALSE`
+#'
+#' @references Brungard et al. (2024) 30 meter Standardized Elevation
+#'   Derivatives of the United States (STEDUS30). Manuscript in preparation.
+#'   Available online:
 #'   https://storage.googleapis.com/dsm-ft-stedus-30m-public/index.html
 #' 
 #' @return A _SpatRaster_ object.
