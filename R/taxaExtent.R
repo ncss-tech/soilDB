57be6132e6b3f9a89a0e63d3d302bac1a8e6412f
Dylan Beaudette <dylanbeaudette@users.noreply.github.com>
2025-10-21 13:27:46 -0700

    support and documentation for family mineralogy classes #415

--- R/taxaExtent.R
+++ R/taxaExtent.R
@@ -1,23 +1,25 @@
 
-#' @title Get SoilWeb 800m Major Component Soil Taxonomy Grids
+#' @title Soil Taxonomy Grids via SoilWeb
 #' 
-#' @description This function downloads a generalized representation of the geographic extent of any single taxon from the top 4 levels of Soil Taxonomy, or taxa matching a given formative element used in Great Group or subgroup taxa. Data are provided by SoilWeb, ultimately sourced from the current SSURGO snapshot. Data are returned as \code{raster} objects representing area proportion falling within 800m cells. Currently area proportions are based on major components only. Data are only available in CONUS and returned using an Albers Equal Area / NAD83(2011) coordinate reference system (EPSG: 5070).
+#' @description This function downloads gridded, generalized representations of the geographic extent of several types of taxonomic information. These types include any single taxon from the top 4 levels of Soil Taxonomy, formative elements used in great group or subgroup taxa, or family mineralogy class. Data are provided by SoilWeb, ultimately sourced from the current SSURGO snapshot. Data are returned as `SpatRaster` objects representing area proportion falling within 800m cells. Currently area proportions are based on major components only. Data are only available in CONUS and returned using an Albers Equal Area / NAD83(2011) coordinate reference system (EPSG: 5070).
 #' 
-#' @param x single taxon label (e.g. `haploxeralfs`) or formative element (e.g. `pale`), case-insensitive
+#' @param x single taxon label (e.g. `haploxeralfs`), formative element (e.g. `pale`), or family mineralogy class (see Details)
+#' 
+#' @param type query type: `taxon`, `formative element`, `mineralogy class`
 #' 
 #' @param level the taxonomic level within the top 4 tiers of Soil Taxonomy, one of `'order'`, `'suborder'`, `'greatgroup'`, `'subgroup'`
 #' 
-#' @param formativeElement logical, search using formative elements instead of taxon label
+#' @param formativeElement no longer used, see `level` argument
 #' 
 #' @param timeout time that we are willing to wait for a response, in seconds
 #' 
 #' @param as_Spatial Return raster (`RasterLayer`) classes? Default: `FALSE`.
 #' 
-#' @return a `SpatRaster` object (or `RasterLayer` when `as_Spatial=TRUE`)
+#' @return a `SpatRaster` object (or `RasterLayer` when `as_Spatial = TRUE`)
 #' 
 #' @author D.E. Beaudette and A.G. Brown
 #' 
-#' @details See the [Geographic Extent of Soil Taxa](https://ncss-tech.github.io/AQP/soilDB/taxa-extent.html) tutorial for more detailed examples.
+#' @details See the [Geographic Extent of Soil Taxa](https://ncss-tech.github.io/AQP/soilDB/taxa-extent.html) tutorial for more detailed examples. The [Keys to Soil Taxonomy](https://www.nrcs.usda.gov/sites/default/files/2022-09/Keys-to-Soil-Taxonomy.pdf) is the authoritative resource for definitions of taxa, formative elements, and family mineralogy classes.
 #' 
 #' ## Taxon Queries
 #' 
@@ -158,7 +160,41 @@
 #' * vitric: (vitric) presence of glassy material
 #' * xanthic: (xanthic) more yellow than typic subgroup
 #' * xeric: (xeric) more xeric than typic subgroup
+#' 
+#' 
+#' ## Family Mineralogy Class Queries
+#' The family mineralogy class grids hosted by SoilWeb represent a simplification; major components with highly contrasting particle size classes (and thus multiple mineralogy classes) will match each class present in the family classification. For example, major components with 'glassy over mixed' and 'glassy' family mineralogy classes are included in the same extent grid. The 'mixed' mineralogy class is not currently included.
+#' 
+#' The following simplified mineralogy classes are available. Descriptions have been condensed for brevity. See chapter 17 from Keys to Soil Taxonomy for full descriptions.
+#' 
+#' * allitic: 18 to 40 percent (by weight) gibbsite in the fine-earth fraction
+#' * amorphic: a. A sum of 8 times the Si (percent by weight extracted by ammonium oxalate from the fine-earth fraction) plus 2 times the Fe (percent by weight extracted by ammonium oxalate from the fine-earth fraction) of 5 or more; and, The product of 8 times the Si is more than the product of 2 times the Fe.
+#' * anhydritic: Any particle-size class and 15 percent or more (by weight) anhydrite, either in the fine-earth fraction or in the fraction less than 20 mm in diameter, whichever has a higher percentage of anhydrite.
+#' * carbonatic: Any particle-size class and more than 40 percent (by weight) carbonates (expressed as CaCO3 ) plus gypsum, either in the fine-earth fraction or in the fraction less than 20 mm in diameter, whichever has a higher percentage of carbonates plus gypsum
+#' * diatomaceous (used for organic soils with limnic materials)
+#' * ferrihydritic: A sum of 8 times the Si (percent by weight extracted by ammonium oxalate from the fine-earth fraction) plus 2 times the Fe (percent by weight extracted by ammonium oxalate from the fine-earth fraction) of 5 or more.
+#' * ferritic: More than 40 percent (by weight) iron oxide as Fe2O3 (more than 28 percent Fe), extractable by dithionite-citrate, in the fine-earth fraction.
+#' * ferruginous: 18 to 40 percent (by weight) iron oxide as Fe2O3 (12.6 to 28 percent Fe), extractable by dithionite-citrate, in the fineearth fraction.
+#' * gibbsitic: More than 40 percent (by weight) gibbsite in the fine earth fraction.
+#' * glassy: 30 percent or more (by grain count) volcanic glass in the 0.02 to 2.0 mm fraction.
+#' * glauconitic: 30 percent or more (by grain count) volcanic glass in the 0.02 to 2.0 mm fraction.
+#' * gypsic: Any particle-size class and 15 percent or more (by weight) gypsum, either in the fine-earth fraction or in the fraction less than 20 mm in diameter, whichever has a higher percentage of gypsum.
+#' * halloysitic: Have more than 50 percent (by weight) halloysite plus kaolinite and allophane and more halloysite than any other single kind of clay mineral.
+#' * hypergypsic: 40 percent or more (by weight) gypsum either in the fine-earth fraction or in the fraction less than 20 mm in diameter, whichever has a higher percentage of gypsum.
+#' * illitic: Have more than 50 percent (by weight) illite (hydrous mica) and commonly more than 4 percent K2O.
+#' * isotic: In more than one-half of the thickness, all of the following: a. No free carbonates; and, b. A sodium fluoride pH (NaF pH) of 8.4 or more; and, c. A ratio of 1500 kPa water to measured clay of 0.6 or more.
+#' * kaolinitic: More than 50 percent (by weight) kaolinite plus halloysite, dickite, nacrite, and other 1:1 or nonexpanding 2:1 layer minerals and gibbsite and less than 10 percent (by weight) smectite minerals (montmorillonite, beidellite, and nontronite) in the fraction less than 0.002 mm in diameter, and more kaolinite than halloysite.
+#' * magnesic: Any particle-size class, except for fragmental, and more than 40 percent (by weight) magnesium-silicate minerals, such as the serpentine minerals (antigorite, chrysotile, and lizardite) plus talc, olivines, Mg-rich pyroxenes, and Mg-rich amphiboles, in the fine-earth fraction.
+#' * micaceous: More than 45 percent (by grain count) mica and stable mica pseudomorphs in the 0.02 to 0.25 mm fraction.
+#' * opaline: 30 percent or more (by weight) diatoms, plant opal, and sponge spicules in the fine-earth fraction.
+#' * parasesquic: A total percent (by weight) iron oxide as Fe2O3 (percent Fe extractable by dithionite-citrate times 1.43) plus the percent (by weight) gibbsite of more than 10 in the fine-earth fraction.
+#' * sesquic: 18 to 40 percent (by weight) iron oxide as Fe2O3 (12.6 to 28 percent Fe), extractable by dithionite-citrate, in the fine-earth fraction; and, 18 to 40 percent (by weight) gibbsite in the fine-earth fraction.
+#' * siliceous: More than 90 percent (by weight or grain count) silica minerals (quartz, chalcedony, or opal) and other resistant minerals in the 0.02 to 2.0 mm fraction.
+#' * smectitic: Have more smectite minerals (montmorillonite, beidellite, and nontronite), by weight, than any other single kind of clay mineral.
+#' * vermiculitic: Have more vermiculite than any other single kind of clay mineral.
+#' 
 #' @export
+#' 
 #' @examplesIf curl::has_internet() && requireNamespace("terra")
 #' \dontrun{
 #'   library(terra)
@@ -181,11 +217,14 @@
 #'   
 #'   # greatgroup formative element
 #'   taxa <- 'psamm'
-#'   x <- taxaExtent(taxa, level = 'greatgroup', formativeElement = TRUE)
+#'   x <- taxaExtent(taxa, level = 'greatgroup', type = 'formative element')
 #'   
 #'   # subgroup formative element
 #'   taxa <- 'abruptic'
-#'   x <- taxaExtent(taxa, level = 'subgroup', formativeElement = TRUE)
+#'   x <- taxaExtent(taxa, level = 'subgroup', type = 'formative element')
+#'   
+#'   # 'glassy' family mineralogy class 
+#'   x <- taxaExtent('glassy', type = 'mineralogy class')
 #'   
 #'   # coarsen for faster plotting
 #'   a <- terra::aggregate(x, fact = 5, na.rm = TRUE)
@@ -193,46 +232,25 @@
 #'   # quick evaluation of the result
 #'   terra::plot(a, axes = FALSE)
 #' }
-taxaExtent <- function(x, level = c('order', 'suborder', 'greatgroup', 'subgroup'),
-                       formativeElement = FALSE, timeout = 60,
-                       as_Spatial = getOption('soilDB.return_Spatial', default = FALSE)) {
+taxaExtent <- function(x, 
+                       type = c('taxon', 'formative element', 'mineralogy class'), 
+                       level = c('order', 'suborder', 'greatgroup', 'subgroup'),
+                       formativeElement = FALSE, 
+                       timeout = 60,
+                       as_Spatial = getOption('soilDB.return_Spatial', default = FALSE)
+                       ) {
  
   ## sanity checks
   
-  # legal levels
+  # arguments with fixed possibilities  
   level <- match.arg(level)
+  type <- match.arg(type)
   
   # base URL
   base.url <- 'https://casoilresource.lawr.ucdavis.edu/taxa-grid-cache'
   
-  # main branch
-  # formative element query
-  if (formativeElement) {
-    
-    # formative elements are only available at the greatgroup / subgroup for now
-    if (level %in% c('order', 'suborder')) {
-      stop('formative element queries are only supported for greatgroup and subgroup taxa', call. = FALSE)
-    }
-    
-    # convert formative element level to path
-    subdir <- switch(
-      level,
-      greatgroup = 'gg',
-      subgroup = 'sg'
-    )
-    
-    # full URL
-    u <- URLencode(
-      sprintf(
-        '%s/fm/%s/%s.tif', 
-        base.url,
-        subdir, 
-        x
-      )
-    )
-    
-  } else {
-    # taxon query
+  # form URL based on query type: taxon, formative element, mineralogy class
+  if(type == 'taxon') {
     
     # encode taxon name: spaces -> underscores
     x <- gsub(pattern = ' ', replacement = '_', x = tolower(x), fixed = TRUE)
@@ -255,8 +273,45 @@ taxaExtent <- function(x, level = c('order', 'suborder', 'greatgroup', 'subgroup
         x
       )
     )
-    
   }
+  
+  if(type == 'formative element') {
+    
+    # formative elements are only available at the greatgroup / subgroup for now
+    if (level %in% c('order', 'suborder')) {
+      stop('formative element queries are only supported for greatgroup and subgroup taxa', call. = FALSE)
+    }
+    
+    # convert formative element level to path
+    subdir <- switch(
+      level,
+      greatgroup = 'gg',
+      subgroup = 'sg'
+    )
+    
+    # full URL
+    u <- URLencode(
+      sprintf(
+        '%s/fm/%s/%s.tif', 
+        base.url,
+        subdir, 
+        x
+      )
+    )
+  }
+  
+  if(type == 'mineralogy class') {
+    
+    # full URL
+    u <- URLencode(
+      sprintf(
+        '%s/minclass/%s.tif', 
+        base.url, 
+        x
+      )
+    )
+  }
+  
   
   # init temp files
   tf <- tempfile(fileext = '.tif')
