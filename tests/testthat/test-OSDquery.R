899df972358e080f24ebac3d03f5fd9543fe792e
Andrew Gene Brown <andrew.g.brown@usda.gov>
2026-02-02 14:38:04 -0800

    fix: OSDquery tests with unhandled httr::POST

--- tests/testthat/test-OSDquery.R
+++ tests/testthat/test-OSDquery.R
@@ -1,48 +1,37 @@
 context("OSDquery() -- requires internet connection")
 
 test_that("OSDquery() works", {
-  
+
   skip_if_not_installed("httr")
   
   skip_if_not_installed("jsonlite")
   
   skip_if_offline()
-
+  
   skip_on_cran()
-
-  # a message is printed and NULL returned when no results
-  res <- suppressMessages(OSDquery(geog_assoc_soils = 'pardee'))
-
-  # if load balancer or server is down, skip tests
-  skip_if(is.null(res))
+  
+  res <- try(suppressMessages(OSDquery(geog_assoc_soils = 'pardee')))
+  
+  skip_if(inherits(res, 'try-error') || is.null(res))
   
   # standard request
   expect_true(inherits(res, 'data.frame'))
-  
   
   # search entire OSD text
-  res <- suppressMessages(OSDquery(everything = 'floodplain', mlra = '18'))
+  res <- try(suppressMessages(OSDquery(everything = 'floodplain', mlra = '18')))
+  
+  skip_if(inherits(res, 'try-error') || is.null(res))
   
   # standard request
   expect_true(inherits(res, 'data.frame'))
-
-})
-
-
-test_that("OSDquery() returns NULL with bogus query", {
   
-  skip_if_not_installed("httr")
-  
-  skip_if_not_installed("jsonlite")
-  
-  skip_if_offline()
-
-  skip_on_cran()
-
   # a message is printed and NULL returned when no results
-  res <- suppressMessages(OSDquery(geog_assoc_soils = 'XXXXXX'))
+  res <- try(suppressMessages(OSDquery(geog_assoc_soils = 'XXXXXX')))
+  
+  skip_if(inherits(res, 'try-error'))
+  
   expect_null(res)
-
+  
 })
 
 
