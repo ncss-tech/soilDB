67155bd268871c8b1294d1830d18d7cf61471d01
Andrew Gene Brown <andrew.g.brown@usda.gov>
2026-04-01 18:00:54 -0700

    fix: empty tests

--- tests/testthat/test-get_SDA_interpretation.R
+++ tests/testthat/test-get_SDA_interpretation.R
@@ -19,6 +19,8 @@ test_that("dynamically determine target row counts", {
   r1 <- SDA_query(sprintf(q, area_in))
   r2a <- SDA_query(sprintf(q, comp_in[1]))
   
+  expect_true(inherits(r1, 'data.frame') || is.null(r1))
+  
   target_area_rows <<- nrow(r1) # 1:1 with mukey
   target_area_rows_all <<- nrow(r2a) # 1:1 with component
 
